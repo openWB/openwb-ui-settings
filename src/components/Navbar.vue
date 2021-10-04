89c784f7b06551abce0c7959b3156ea50b18c3b4
Lutz Bender <lutz.bender@openwb.de>
2021-10-04 17:27:22 +0200

    fix navbar / change output dir

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
 	<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-		<a class="navbar-brand" href="/">
+		<a class="navbar-brand" href="/openWB/web/">
 			<span>openWB</span>
 		</a>
 		<button
