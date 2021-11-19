cae3d14e0ce6655ab4cab1fd047825ee357e7709
Lutz Bender <lutz.bender@openwb.de>
2021-11-19 10:12:56 +0100

    update mqtt component descriptions

--- src/components/devices/mqtt/inverter.vue
+++ src/components/devices/mqtt/inverter.vue
@@ -5,7 +5,25 @@
 			<span class="small">(Modul: {{ $options.name }})</span>
 		</openwb-base-heading>
 		<openwb-base-alert subtype="info">
-			{{ deviceId }} / {{ componentId }}
+			DeviceId: {{ deviceId }} / ComponentId: {{ componentId }}<br />
+			Alle Zahlen mit Nachkommastellen sind mit einem Punkt als
+			Dezimaltrenner zu senden!
+			<ul>
+				<li>
+					<span class="text-info"
+						>openWB/set/pv/{{ componentId }}/power</span
+					><br />
+					PV-Leistung in Watt, ohne Nachkommastellen (Integer), nur
+					negativ!
+				</li>
+				<li>
+					<span class="text-info"
+						>openWB/set/pv/{{ componentId }}/counter</span
+					><br />
+					Erzeugte Energie in Wh, mit Nachkommastellen (Float), nur
+					positiv
+				</li>
+			</ul>
 		</openwb-base-alert>
 	</div>
 </template>
