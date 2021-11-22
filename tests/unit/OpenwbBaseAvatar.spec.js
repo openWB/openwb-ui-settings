c6604df7c881cf8ea4c9804c1207a2b1ee3b0d95
Lutz Bender <lutz.bender@openwb.de>
2021-11-22 08:31:50 +0100

    introduce unit tests

---
+++ tests/unit/OpenwbBaseAvatar.spec.js
@@ -0,0 +1,15 @@
+import { shallowMount } from "@vue/test-utils";
+import OpenwbBaseAvatar from "@/components/OpenwbBaseAvatar.vue";
+
+describe("OpenwbBaseAvatar.vue", () => {
+	it("render slot when passed", () => {
+		const slotContent = "!";
+		const wrapper = shallowMount(OpenwbBaseAvatar, {
+			slots: {
+				default: slotContent,
+			},
+		});
+		const mainDiv = wrapper.find("div.avatar");
+		expect(mainDiv.html()).toContain(slotContent);
+	});
+});
---
+++ tests/unit/OpenwbBaseHeading.spec.js
@@ -0,0 +1,25 @@
+import { shallowMount } from "@vue/test-utils";
+import OpenwbBaseHeading from "@/components/OpenwbBaseHeading.vue";
+
+describe("OpenwbBaseHeading.vue", () => {
+	it("render default slot when passed", () => {
+		const slotContent = "<span>Default slot content.</span>";
+		const wrapper = shallowMount(OpenwbBaseHeading, {
+			slots: {
+				default: slotContent,
+			},
+		});
+		const heading = wrapper.find("div.card-text-heading");
+		expect(heading.html()).toContain(slotContent);
+	});
+	it("render actions slot when passed", () => {
+		const slotContent = "<span>Actions slot content.</span>";
+		const wrapper = shallowMount(OpenwbBaseHeading, {
+			slots: {
+				actions: slotContent,
+			},
+		});
+		const headingActions = wrapper.find("span.actions");
+		expect(headingActions.html()).toContain(slotContent);
+	});
+});
