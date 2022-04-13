7df7e543585d6238a37e81bb565889bf5cd0a464
Lutz Bender <user.email=github@lutz-bender.de>
2022-04-13 15:33:05 +0200

    updated dependencies

--- vue.config.js
+++ vue.config.js
@@ -19,8 +19,12 @@ module.exports = {
 			}),
 		],
 		output: {
-			filename: process.env.VUE_CLI_MODERN_BUILD ? 'js/[name].[contenthash:8].js' : 'js/[name]-legacy.[contenthash:8].js',
-			chunkFilename: process.env.VUE_CLI_MODERN_BUILD ? 'js/[name].[contenthash:8].js' : 'js/[name]-legacy.[contenthash:8].js',
+			filename: process.env.VUE_CLI_MODERN_BUILD
+				? "js/[name].[contenthash:8].js"
+				: "js/[name]-legacy.[contenthash:8].js",
+			chunkFilename: process.env.VUE_CLI_MODERN_BUILD
+				? "js/[name].[contenthash:8].js"
+				: "js/[name]-legacy.[contenthash:8].js",
 		},
 	},
 };
