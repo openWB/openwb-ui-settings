e54230232a2a0144aa375b5cc3305f8825196ef0
Lutz Bender <github@lutz-bender.de>
2022-06-27 12:35:10 +0200

    fix naming of charge points in charts

--- src/views/MonthlyChart.vue
+++ src/views/MonthlyChart.vue
@@ -728,7 +728,7 @@ export default {
 					case "pv":
 						return "Wechselrichter";
 					case "cp":
-						return "Ladepunkt";
+						return "Ladepunkte";
 					default:
 						console.warn("unknown group key:", groupKey);
 				}
