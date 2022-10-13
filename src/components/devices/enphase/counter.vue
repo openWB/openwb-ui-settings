44fe9a155e0bb6a62fdf7e95b1f3a41a2801060d
LKuemmel <lena.kuemmel@openwb.de>
2022-10-13 13:41:28 +0200

    add settings for enphase and siemens sentron

---
+++ src/components/devices/enphase/counter.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="device-enphase-counter">
+		<openwb-base-heading>
+			Einstellungen für Enphase Envoy / IQ Gateway Zähler
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-number-input
+			title="EID"
+			required
+			:model-value="configuration.eid"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.eid')
+			"
+		>
+			<template #help> EID für "net-consumption" </template>
+		</openwb-base-number-input>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceEnphaseCounter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
---
+++ src/components/devices/enphase/device.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="device-enphase">
+		<openwb-base-heading>
+			Einstellungen für Enphase Envoy / IQ Gateway
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-alert subtype="info">
+			Geräte mit Firmware-Versionen 7.0 oder neuer werden derzeit nicht
+			unterstützt.
+		</openwb-base-alert>
+		<openwb-base-text-input
+			title="IP oder Hostname"
+			subtype="host"
+			required
+			:model-value="configuration.hostname"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.hostname')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceEnphase",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
