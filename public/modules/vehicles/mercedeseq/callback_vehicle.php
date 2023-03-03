e86759a6347021d765b433e6f7b026ab68a16140
Lutz Bender <github@lutz-bender.de>
2023-03-03 11:13:19 +0100

    fix callback_url

--- public/modules/vehicles/mercedeseq/callback_vehicle.php
+++ public/modules/vehicles/mercedeseq/callback_vehicle.php
@@ -14,9 +14,10 @@
 		$code = escapeshellarg($_GET['code']);
 
 		$port = ($_SERVER['SERVER_PORT'] != 80 && $_SERVER['SERVER_PORT'] != 443) ? ':' . $_SERVER['SERVER_PORT'] : '';
-		$callback_url = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . $port . $_SERVER['REQUEST_URI'];
+		$callback_url = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['HTTP_HOST'] . $port . $_SERVER['SCRIPT_NAME'];
 
-		system(join(" ", [$command, $state, $code, $callback_url]));
+		$system_command = join(" ", [$command, $state, $code, $callback_url]);
+		system($system_command);
 	} else {
 		echo "<p>" . $_GET["error"] . "</p>";
 		echo "<p>" . $_GET["error_description"] . "</p>";
