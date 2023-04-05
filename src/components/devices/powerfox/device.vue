66b0e7be776f51586fd34fa30b8968caa281caa2
LKuemmel <lena.kuemmel@openwb.de>
2023-04-05 16:36:36 +0200

    Powerfox

---
+++ src/components/devices/powerfox/device.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="device-powerfox">
+		<openwb-base-heading>
+			Einstellungen für Powerfox
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="Benutzername"
+			subtype="user"
+			required
+			:model-value="configuration.user"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.user')
+			"
+		/>
+		<openwb-base-text-input
+			title="Passwort"
+			subtype="password"
+			required
+			:model-value="configuration.password"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.password')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DevicePowerfox",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
