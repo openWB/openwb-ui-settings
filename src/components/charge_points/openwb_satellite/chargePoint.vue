a77886c1bf34c477393e445753e30270646c5188
LKuemmel <lena.kuemmel@openwb.de>
2023-05-04 12:05:50 +0200

    Settings

--- src/components/charge_points/openwb_satellite/chargePoint.vue
+++ src/components/charge_points/openwb_satellite/chargePoint.vue
@@ -7,34 +7,14 @@
 			:model-value="configuration.ip_address"
 			@update:model-value="updateConfiguration($event, 'ip_address')"
 		/>
-		<openwb-base-number-input
-			title="Modbus-ID der EVSE"
-			required
-			:min="1"
-			:max="255"
-			:model-value="configuration.modbus_id_evse"
-			@update:model-value="updateConfiguration($event, 'modbus_id_evse')"
-		/>
 		<openwb-base-select-input
 			title="Zählertyp"
-			notSelected="Bitte auswählen"
 			:options="[
-				{ value: 'mpm3pm', text: 'MPMP3PM' },
 				{ value: 'sdm630', text: 'SDM630' },
 			]"
 			:model-value="configuration.counter_type"
 			@update:model-value="
 				updateConfiguration($event, 'configuration.counter_type')
-			"
-		/>
-		<openwb-base-number-input
-			title="Modbus-ID des Zählers"
-			required
-			:min="1"
-			:max="255"
-			:model-value="configuration.modbus_id_counter"
-			@update:model-value="
-				updateConfiguration($event, 'modbus_id_counter')
 			"
 		/>
 	</div>
