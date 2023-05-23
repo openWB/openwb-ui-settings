76a38187ee15081180495bafd1aabfa67046c586
LKuemmel <lena.kuemmel@openwb.de>
2023-05-23 13:38:39 +0200

    settings for kostal sem

---
+++ src/components/devices/kostal_sem/device.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="device-kostal_sem">
+		<openwb-base-heading>
+			Einstellungen für Kostal Smart Energy Meter oder TQ EM 410
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="IP oder Hostname"
+			subtype="host"
+			required
+			:model-value="configuration.ip_address"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.ip_address')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceKostalSEM",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
