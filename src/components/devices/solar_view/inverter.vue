9fd23435d4c1deb0aaed73177f356be993b2b475
LKuemmel <lena.kuemmel@openwb.de>
2023-05-23 13:54:12 +0200

    settings SolarView

---
+++ src/components/devices/solar_view/inverter.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="device-solar_view-inverter">
+		<openwb-base-heading>
+			Einstellungen für SolarView Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="Kommando für die Abfrage"
+			required
+			:model-value="configuration.command"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.command')
+			"
+		>
+			<template #help>
+				Kommandos gemäß SolarView-Dokumentation, z.B.: 00* (gesamte Anlage), 01* (Wechselrichter 1), 02* (Wechselrichter 2)
+			</template>
+		</openwb-base-text-input>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceSolarViewInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
