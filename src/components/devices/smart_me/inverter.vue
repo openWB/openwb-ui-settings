50b9733262b631e0a49125e2d3aeccd0a3ca6ab4
LKuemmel <lena.kuemmel@openwb.de>
2023-05-23 13:45:34 +0200

    settings smart-me

---
+++ src/components/devices/smart_me/inverter.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="device-smart_me-inverter">
+		<openwb-base-heading>
+			Einstellungen für smart-me Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="Geräte-ID"
+			required
+			:model-value="configuration.id"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.id')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceSmartMeInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
