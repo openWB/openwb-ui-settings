263c788ad15b187e648e9bc7d627363550f1f5cd
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2023-06-13 10:30:31 +0200

    Kostal Steca, FEMS, RCT (#287)
    * Kostal Steca
    
    * fems
    
    * rct

---
+++ src/components/devices/kostal_steca/inverter.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="device-kostal-steca-inverter">
+		<openwb-base-heading>
+			Einstellungen für Kostal Piko MP oder Steca Grid Coolcept Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+				<openwb-base-button-group-input
+			title="Variante"
+			:buttons="[
+				{ buttonValue: false, text: 'Kostal Piko MP (non-plus)' },
+				{ buttonValue: true, text: 'Kostal Piko MP oder Steca Grid Coolcept' },
+			]"
+			:model-value="configuration.variant_steca"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.variant_steca')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceKostalStecaInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
