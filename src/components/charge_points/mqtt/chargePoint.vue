9b40537abc009223b0999aca038bcd1d53a0eb52
Lutz Bender <github@lutz-bender.de>
2023-07-04 19:58:00 +0200

    add info for mqtt charge point module

--- src/components/charge_points/mqtt/chargePoint.vue
+++ src/components/charge_points/mqtt/chargePoint.vue
@@ -1,5 +1,10 @@
 <template>
 	<div class="charge-point-mqtt">
+		<openwb-base-alert subtype="danger">
+			Dieser Ladepunkt-Typ darf nur in Kombination mit dem Simulator
+			zur Fehleranalyse genutzt werden. Eine andere Verwendung ist
+			laut den Nutzungsbedingungen untersagt.
+		</openwb-base-alert>
 		<openwb-base-alert subtype="info">
 			Das MQTT Modul stellt keine aktive Verbindung her, sondern erwartet,
 			dass die Daten von speziellen Topics im Broker gelesen bzw.
