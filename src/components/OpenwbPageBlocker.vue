513f2a1a368a0c11e61c4d717013c8aa6cece214
LKuemmel <lena.kuemmel@openwb.de>
2023-08-02 12:45:39 +0200

    fix page blocker

--- src/components/OpenwbPageBlocker.vue
+++ src/components/OpenwbPageBlocker.vue
@@ -64,7 +64,7 @@ export default {
 			if (
 				this.$store.state.mqtt["openWB/system/boot_done"] == undefined
 			) {
-				return false;
+				return true;
 			}
 			return !this.$store.state.mqtt["openWB/system/boot_done"];
 		},
