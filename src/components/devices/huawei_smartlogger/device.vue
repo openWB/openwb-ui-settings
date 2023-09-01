c1755f92687ef1ff727c4da5ca43c5196553e2ae
Lutz Bender <github@lutz-bender.de>
2023-09-01 08:03:59 +0200

    add huawei smart logger configuration ui

---
+++ src/components/devices/huawei_smartlogger/device.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="device-huawei-smart-logger">
+		<openwb-base-heading>
+			Einstellungen für Huawei SmartLogger
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="IP oder Hostname"
+			subtype="host"
+			required
+			:model-value="configuration.ip_address"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.ip_address')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceHuaweiSmartLogger",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
