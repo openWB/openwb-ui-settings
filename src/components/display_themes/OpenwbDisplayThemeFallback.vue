c8a33a5ad8afd4e5113287547e7d29f75764a5e8
Lutz Bender <github@lutz-bender.de>
2023-10-02 12:16:17 +0200

    update configuration fallback modules

--- src/components/display_themes/OpenwbDisplayThemeFallback.vue
+++ src/components/display_themes/OpenwbDisplayThemeFallback.vue
@@ -1,27 +1,36 @@
 <template>
-	<div
-		class="display-theme-fallback"
-		v-if="Object.keys(configuration).length > 0"
-	>
-		<openwb-base-alert subtype="warning">
-			Es wurde keine Konfigurationsseite für das Display Theme "{{
-				displayThemeType
-			}}" gefunden. Die Einstellungen können als JSON direkt bearbeitet
-			werden.
-		</openwb-base-alert>
-		<openwb-base-textarea
-			title="Konfiguration"
-			subtype="json"
-			:model-value="configuration"
-			@update:model-value="updateConfiguration($event, 'configuration')"
+	<div class="display-theme-fallback">
+		<openwb-base-alert
+			v-if="Object.keys(configuration).length == 0"
+			subtype="info"
 		>
-			<template #help>
-				Bitte prüfen Sie, ob die Eingaben richtig interpretiert werden.
-			</template>
-		</openwb-base-textarea>
-		<openwb-base-alert subtype="info">
-			<pre>{{ JSON.stringify(configuration, undefined, 2) }}</pre>
+			Das Display-Theme "{{ displayThemeType }}" bietet keine
+			Einstellungen.
 		</openwb-base-alert>
+		<div v-else>
+			<openwb-base-alert subtype="warning">
+				Es wurde keine Konfigurationsseite für das Display-Theme "{{
+					displayThemeType
+				}}" gefunden. Die Einstellungen können als JSON direkt
+				bearbeitet werden.
+			</openwb-base-alert>
+			<openwb-base-textarea
+				title="Konfiguration"
+				subtype="json"
+				:model-value="configuration"
+				@update:model-value="
+					updateConfiguration($event, 'configuration')
+				"
+			>
+				<template #help>
+					Bitte prüfen Sie, ob die Eingaben richtig interpretiert
+					werden.
+				</template>
+			</openwb-base-textarea>
+			<openwb-base-alert subtype="info">
+				<pre>{{ JSON.stringify(configuration, undefined, 2) }}</pre>
+			</openwb-base-alert>
+		</div>
 	</div>
 	<openwb-base-alert v-else subtype="info">
 		Das Display Theme "{{ displayThemeType }}" kann nicht angepasst werden.
