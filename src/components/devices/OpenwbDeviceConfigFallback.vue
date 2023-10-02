5028f4a7cf0a99e39680b84749d93bde68ae9642
Lutz Bender <github@lutz-bender.de>
2023-10-02 12:16:17 +0200

    update configuration fallback modules

--- src/components/devices/OpenwbDeviceConfigFallback.vue
+++ src/components/devices/OpenwbDeviceConfigFallback.vue
@@ -1,27 +1,42 @@
 <template>
 	<div class="device-fallback">
-		<openwb-base-alert subtype="warning">
-			Es wurde keine Konfigurationsseite für den
-			<span v-if="componentType">
-				Komponenten-Typ "{{ componentType }}"
-			</span>
-			<span v-else>Geräte-Typ "{{ deviceType }}"</span>
-			gefunden. Die Einstellungen können als JSON direkt bearbeitet
-			werden.
-		</openwb-base-alert>
-		<openwb-base-textarea
-			title="Konfiguration"
-			subtype="json"
-			:model-value="configuration"
-			@update:model-value="updateConfiguration($event, 'configuration')"
+		<openwb-base-alert
+			v-if="Object.keys(configuration).length == 0"
+			subtype="info"
 		>
-			<template #help>
-				Bitte prüfen Sie, ob die Eingaben richtig interpretiert werden.
-			</template>
-		</openwb-base-textarea>
-		<openwb-base-alert subtype="info">
-			<pre>{{ JSON.stringify(configuration, undefined, 2) }}</pre>
+			<span v-if="componentType">
+				Der Komponenten-Typ "{{ componentType }}"
+			</span>
+			<span v-else>Der Gerät-Typ "{{ deviceType }}"</span>
+			bietet keine Einstellungen.
 		</openwb-base-alert>
+		<div v-else>
+			<openwb-base-alert subtype="warning">
+				Es wurde keine Konfigurationsseite für den
+				<span v-if="componentType">
+					Komponenten-Typ "{{ componentType }}"
+				</span>
+				<span v-else>Geräte-Typ "{{ deviceType }}"</span>
+				gefunden. Die Einstellungen können als JSON direkt bearbeitet
+				werden.
+			</openwb-base-alert>
+			<openwb-base-textarea
+				title="Konfiguration"
+				subtype="json"
+				:model-value="configuration"
+				@update:model-value="
+					updateConfiguration($event, 'configuration')
+				"
+			>
+				<template #help>
+					Bitte prüfen Sie, ob die Eingaben richtig interpretiert
+					werden.
+				</template>
+			</openwb-base-textarea>
+			<openwb-base-alert subtype="info">
+				<pre>{{ JSON.stringify(configuration, undefined, 2) }}</pre>
+			</openwb-base-alert>
+		</div>
 	</div>
 </template>
 
--- src/components/display_themes/OpenwbDisplayThemeFallback.vue
+++ src/components/display_themes/OpenwbDisplayThemeFallback.vue
@@ -1,24 +1,36 @@
 <template>
 	<div class="display-theme-fallback">
-		<openwb-base-alert subtype="warning">
-			Es wurde keine Konfigurationsseite für das Display Theme "{{
-				displayThemeType
-			}}" gefunden. Die Einstellungen können als JSON direkt bearbeitet
-			werden.
-		</openwb-base-alert>
-		<openwb-base-textarea
-			title="Konfiguration"
-			subtype="json"
-			:model-value="configuration"
-			@update:model-value="updateConfiguration($event, 'configuration')"
+		<openwb-base-alert
+			v-if="Object.keys(configuration).length == 0"
+			subtype="info"
 		>
-			<template #help>
-				Bitte prüfen Sie, ob die Eingaben richtig interpretiert werden.
-			</template>
-		</openwb-base-textarea>
-		<openwb-base-alert subtype="info">
-			<pre>{{ JSON.stringify(configuration, undefined, 2) }}</pre>
+			Das Display-Theme "{{ displayThemeType }}" bietet keine
+			Einstellungen.
 		</openwb-base-alert>
+		<div v-else>
+			<openwb-base-alert subtype="warning">
+				Es wurde keine Konfigurationsseite für das Display-Theme "{{
+					displayThemeType
+				}}" gefunden. Die Einstellungen können als JSON direkt
+				bearbeitet werden.
+			</openwb-base-alert>
+			<openwb-base-textarea
+				title="Konfiguration"
+				subtype="json"
+				:model-value="configuration"
+				@update:model-value="
+					updateConfiguration($event, 'configuration')
+				"
+			>
+				<template #help>
+					Bitte prüfen Sie, ob die Eingaben richtig interpretiert
+					werden.
+				</template>
+			</openwb-base-textarea>
+			<openwb-base-alert subtype="info">
+				<pre>{{ JSON.stringify(configuration, undefined, 2) }}</pre>
+			</openwb-base-alert>
+		</div>
 	</div>
 </template>
 
