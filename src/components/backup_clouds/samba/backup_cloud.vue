075a66ce947b98c782fbdf50cc7cea082737085b
Lutz Bender <github@lutz-bender.de>
2023-10-20 11:46:38 +0200

    add samba configuration

---
+++ src/components/backup_clouds/samba/backup_cloud.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="backup-cloud-samba">
+		<openwb-base-heading>
+			Einstellungen für Samba-Backup Cloud
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="Pfad zur Freigabe"
+			required
+			:model-value="configuration.smb_path"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.smb_path')
+			"
+		/>
+		<openwb-base-text-input
+			title="Benutzer"
+			:model-value="configuration.smb_user"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.smb_user')
+			"
+		/>
+		<openwb-base-text-input
+			title="Kennwort"
+			subtype="password"
+			:model-value="configuration.smb_password"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.smb_password')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "BackupCloudSamba",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+	},
+	data() {
+		return {};
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
