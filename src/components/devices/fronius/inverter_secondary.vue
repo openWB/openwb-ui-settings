eff866d50366997d506ccb70d28e004ef2280df7
LKuemmel <lena.kuemmel@openwb.de>
2023-10-23 13:49:20 +0200

    Fronius zusätzlicher Wechselrichter

---
+++ src/components/devices/fronius/inverter_secondary.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="device-fronius-inverter">
+		<openwb-base-heading>
+			Einstellungen für einen zusätzlichen Fronius Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-number-input
+			title="ID"
+			:model-value="configuration.id"
+			min="0"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.id')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceFroniusInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
