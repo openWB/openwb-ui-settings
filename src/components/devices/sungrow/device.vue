5d6d0b1cc20e89f44155ce7c371a3339fcc998be
Mantelinho <mantelinho@yahoo.com>
2024-01-11 16:29:08 +0100

    Moved version of sungrow inverters to device level

--- src/components/devices/sungrow/device.vue
+++ src/components/devices/sungrow/device.vue
@@ -6,7 +6,7 @@
 		</openwb-base-heading>
 		<openwb-base-alert subtype="info">
 			Bitte zur Fehlervermeidung die Firmware des Sungrow Wechselrichters
-			und WiNet-S Dongels aktuell halten.
+			und WiNet-S Dongles aktuell halten.
 		</openwb-base-alert>
 		<openwb-base-text-input
 			title="IP oder Hostname"
@@ -37,6 +37,32 @@
 				updateConfiguration($event, 'configuration.modbus_id')
 			"
 		/>
+		<openwb-base-select-input
+			title="Version"
+			:options="[
+				{ value: 0, text: 'SH (Hybrid)' },
+				{ value: 1, text: 'SH (Hybrid) über WiNet-S Dongle' },
+				{ value: 2, text: 'SG (kein Hybrid)' },
+				{ value: 3, text: 'SG (kein Hybrid) über WiNet-S Dongle' },
+			]"
+			:model-value="configuration.version"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.version')
+			"
+		>
+			<template #help>
+				Die Variante SH sollte möglichst über den internen LAN-Port
+				genutzt werden (befindet sich am Wechselrichter hinter dem
+				WiNet-S Dongle), da nur hier die netto AC-Leistung ausgelesen
+				werden kann. Den WiNet-S Dongle zusätzlich ins Heimnetz (per
+				Lan oder Wlan) einbinden, um iSolarCloud nutzen zu können.
+				Wird der Dongle auch für openWB genutzt, kann nur die brutto
+				DC-Leistung angezeigt werden. In diesem Fall muss das Gerät
+				wie ein nicht-hybrider Wechselrichter in der Struktur unter
+				"Lastmanagement" eingerichtet werden, mit dem Nachteil, dass
+				Gesamt-PV-Leistung und Zählerstände ungenau angezeigt werden.
+			</template>
+		</openwb-base-select-input>
 	</div>
 </template>
 
