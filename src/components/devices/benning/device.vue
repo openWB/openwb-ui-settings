2b23c4444a4c01ea7ac3a66f00589c4e3b74e18b
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2024-03-26 13:21:04 +0100

    benning (#411)
    * benning
    
    * npm lint
    
    * openDTU
    
    ---------
    
    Co-authored-by: LKuemmel <lena.kuemmel@openwb.de>

---
+++ src/components/devices/benning/device.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="device-benning">
+		<openwb-base-heading>
+			Einstellungen für Benning
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="IP oder Hostname"
+			subtype="host"
+			required
+			:model-value="configuration.url"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.url')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceBenning",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
