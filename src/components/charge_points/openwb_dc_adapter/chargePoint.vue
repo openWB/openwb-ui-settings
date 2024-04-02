1c7dd1442599c34d76dbc6ea0740705550746f87
Lutz Bender <github@lutz-bender.de>
2024-04-02 14:53:51 +0200

    module configuration
    charge point configuration card
    
    charge point template
    
    vehicle configuration
    
    layout changes
    
    transfer values
    
    fixes
    
    min current dc must be > 0kW

---
+++ src/components/charge_points/openwb_dc_adapter/chargePoint.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="charge-point-openwbdcadapter">
+		<openwb-base-text-input
+			title="IP oder Hostname"
+			subtype="host"
+			required
+			:model-value="configuration.ip_address"
+			@update:model-value="updateConfiguration($event, 'ip_address')"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ChargePointOpenwbDcAdapter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		chargePointId: { default: undefined },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			if (path) {
+				path = "configuration." + path;
+			}
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
