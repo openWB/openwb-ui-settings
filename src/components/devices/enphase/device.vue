b883b78d39068a5310fb281c1a29925ac4de1bca
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2024-04-08 13:58:23 +0200

    Feature enphase (#441)
    * add enphase user/pass to configuration
    
    * add version and auth settings
    
    * add help texts to device
    
    * enphase: added serial number config field
    
    * enphase: added serial number requirement to token help field
    
    ---------
    
    Co-authored-by: Lutz Bender <github@lutz-bender.de>
    Co-authored-by: Stefan-Olt <stefan-oltmanns@gmx.net>

--- src/components/devices/enphase/device.vue
+++ src/components/devices/enphase/device.vue
@@ -4,10 +4,6 @@
 			Einstellungen für Enphase Envoy / IQ Gateway
 			<span class="small">(Modul: {{ $options.name }})</span>
 		</openwb-base-heading>
-		<openwb-base-alert subtype="info">
-			Geräte mit Firmware-Versionen 7.0 oder neuer werden derzeit nicht
-			unterstützt.
-		</openwb-base-alert>
 		<openwb-base-text-input
 			title="IP oder Hostname"
 			subtype="host"
@@ -16,7 +12,81 @@
 			@update:model-value="
 				updateConfiguration($event, 'configuration.hostname')
 			"
+		>
+			<template #help>
+				Bitte geben Sie die IP-Adresse oder den Hostnamen des Enphase
+				Envoy oder IQ Gateway an. Die Info finden Sie in Ihrem Router.
+				Ab Werk ist der Hostname "envoy" oder "envoy.local".
+			</template>
+		</openwb-base-text-input>
+		<openwb-base-select-input
+			title="Version"
+			required
+			:options="[
+				{ value: 1, text: 'Firmware < 7.0' },
+				{ value: 2, text: 'Firmware ab 7.0' },
+			]"
+			:model-value="configuration.version"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.version')
+			"
+		>
+			<template #help>
+				Bitte wählen Sie die Version des Enphase Envoy oder IQ Gateway
+				aus. Die Version 1 ist für Geräte mit einer Firmware-Version
+				unter 7.0, die Version 2 für Geräte mit einer Firmware-Version
+				ab 7.0. Ein angebundener Speicher wird nur bei Version 2
+				unterstützt.
+			</template>
+		</openwb-base-select-input>
+		<openwb-base-text-input
+			v-if="configuration.version > 1"
+			title="Benutzer"
+			subtype="user"
+			:model-value="configuration.user"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.user')
+			"
 		/>
+		<openwb-base-text-input
+			v-if="configuration.version > 1"
+			title="Kennwort"
+			subtype="password"
+			:model-value="configuration.password"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.password')
+			"
+		/>
+		<openwb-base-text-input
+			v-if="configuration.version > 1"
+			title="Envoy Seriennummer"
+			:model-value="configuration.serial"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.serial')
+			"
+		/>
+		<openwb-base-text-input
+			v-if="configuration.version > 1"
+			title="Token"
+			subtype="password"
+			:model-value="configuration.token"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.token')
+			"
+		>
+			<template #help>
+				Wenn Benutzer, Kennwort und Seriennummer des Envoys angegeben
+				werden, wird das Token automatisch beim Speichern abgerufen.
+				Ebenfalls wird ein abgelaufenes Token (derzeit nach einem Jahr)
+				automatisch erneuert.<br />
+				Wenn Sie ein Token manuell erstellen möchten, können Sie dies
+				auf der <a
+					href="https://developer.enphase.com/docs/quickstart.html"
+					target="_blank"
+					rel="noopener"
+				>Enphase-Webseite</a> erledigen.
+			</template>
+		</openwb-base-text-input>
 	</div>
 </template>
 
