91f26bd14e6bc44b4ea5fb887cf20b0947fd717f
pama87 <patrick.mayer@openwb.de>
2024-04-11 11:50:26 +0200

    new module azzurro-zcs/sofar

---
+++ src/components/devices/azzurro_zcs/device.vue
@@ -0,0 +1,53 @@
+<template>
+	<div class="device-azzurro_zcs">
+		<openwb-base-heading>
+			Einstellungen für Azzurro ZCS
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-text-input
+			title="IP oder Hostname"
+			subtype="host"
+			required
+			:model-value="configuration.ip_address"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.ip_address')
+			"
+		/>
+		<openwb-base-number-input
+			title="Port"
+			required
+			:min="1"
+			:max="65535"
+			:model-value="configuration.port"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.port')
+			"
+		/>
+		<openwb-base-number-input
+			title="Modbus ID"
+			required
+			:model-value="configuration.modbus_id"
+			min="1"
+			max="255"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.modbus_id')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceZCS",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
---
+++ src/components/devices/azzurro_zcs/inverter.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="device-azzurro_zcs-inverter">
+		<openwb-base-heading>
+			Einstellungen für Azzurro ZCS Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-alert subtype="info">
+			Diese Komponente benötigt keine Einstellungen.
+		</openwb-base-alert>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceZCSInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
