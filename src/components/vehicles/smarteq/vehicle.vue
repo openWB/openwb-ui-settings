8435f6e6620e57db1de1cb7ef7842611e9f645af
rleidner <89418596+rleidner@users.noreply.github.com>
2024-04-25 15:51:00 +0200

    changed indent as requested

--- src/components/vehicles/smarteq/vehicle.vue
+++ src/components/vehicles/smarteq/vehicle.vue
@@ -79,27 +79,27 @@
 			"
 		>
 			<template #help>
-                                <div>
-				<b>SOC-Log Filtereinstellung</b><br>
-				In Einstellungen-System-Fehlersuche: Debug-Level=Info<br>
-				Mögliche Log-Filter:<br>
-				<ul>
-				<li>f: fetch_soc</li>
-				<li>i: initialization</li>
-				<li>m: websocket message</li>
-				<li>M: websocket message details</li>
-				<li>q: mqtt logging</li>
-				<li>Q: mqtt logging details</li>
-				<li>r: smart eq rest api</li>
-				<li>s: state changes</li>
-				<li>t: token refresh</li>
-				<li>T: token refresh details</li>
-				<li>u: authentication</li>
-				<li>U: authentication details</li>
-				<li>w: webSocket interface</li>
-				<li>W: webSocket interface details</li>
-				</ul>
-                                </div>
+				<div>
+					<b>SOC-Log Filtereinstellung</b><br>
+					In Einstellungen-System-Fehlersuche: Debug-Level=Info<br>
+					Mögliche Log-Filter:<br>
+					<ul>
+						<li>f: fetch_soc</li>
+						<li>i: initialization</li>
+						<li>m: websocket message</li>
+						<li>M: websocket message details</li>
+						<li>q: mqtt logging</li>
+						<li>Q: mqtt logging details</li>
+						<li>r: smart eq rest api</li>
+						<li>s: state changes</li>
+						<li>t: token refresh</li>
+						<li>T: token refresh details</li>
+						<li>u: authentication</li>
+						<li>U: authentication details</li>
+						<li>w: webSocket interface</li>
+						<li>W: webSocket interface details</li>
+					</ul>
+                               	</div>
                         </template>
 		</openwb-base-text-input>
 	</div>
