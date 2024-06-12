fbee272f4ea410d6fe55d2de4a754f46ed6bd675
pama87 <163103468+pama87@users.noreply.github.com>
2024-06-12 11:54:52 +0200

    Boersenmodul (#451)
    * #Boersenstrompreise allgemein energy-charts mit Grundpreisoption
    
    * #Laenderauswahl fix
    
    * #price fix
    
    * #added servePrice via configuration
    
    * #added servePrice to GeneralChargeConfig
    
    * #fix path to ComponentState
    
    * #removed topics and countries
    
    * #removed topic
    
    * #add mixin
    
    * # change variable name, add help text, corrected format
    
    * # requested changes
    
    * # requested change

---
+++ src/components/electricity_tariffs/energycharts/electricity_tariff.vue
@@ -0,0 +1,63 @@
+<template>
+	<div class="electricity-tariff-energycharts">
+		<openwb-base-alert subtype="info">
+			Börsenstrompreise von Energy Charts (energy-charts.info)<br />
+			Die Rohdaten werden von Wissenschaftlern des Fraunhofer-Institut
+			für Solare Energiesysteme ISE aus zahlreichen Quellen stündlich
+			oder täglich abgerufen und für die Darstellung aufbereitet.
+		</openwb-base-alert>
+		<openwb-base-select-input
+			title="Land"
+			notSelected="Bitte auswählen"
+			:options="[
+				{ value: 'DE-LU', text: 'Deutschland' },
+				{ value: 'AT', text: 'Österreich' },
+				{ value: 'CH', text: 'Schweiz' },
+			]"
+			:model-value="electricityTariff.configuration.country"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.country')
+			"
+		/>
+		<openwb-base-number-input
+			title="Aufschlag zum Börsenstrompreis"
+			:min=0
+			:step="0.001"
+			:precision="3"
+			unit="ct/kWh"
+			:model-value="electricityTariff.configuration.surcharge"
+			@update:model-value="
+				updateConfiguration(parseFloat(($event).toFixed(3)), 'configuration.surcharge')
+			"
+		>
+			<template #help>
+				Die Auswahl (Energy-Charts) kann für alle Stromanbieter ausgewählt werden,
+				die die Börsenstrompreise in einem variablen/dynamischen Tarif an die Kunden
+				weitergeben. Das Modul soll es ermöglichen, dass zu tagesaktuellen Strompreisen
+				unter Berücksichtigung eines Aufschlags zu günstigen Zeiten automatisch geladen
+				werden kann und somit die Preisschwankungen an der Börse optimal ausgenutzt werden.
+			</template>
+		</openwb-base-number-input>
+	</div>
+</template>
+
+<script>
+import ComponentState from "/src/components/mixins/ComponentState.vue";
+export default {
+	name: "ElectricityTariffEnergyCharts",
+	mixins: [ComponentState],
+	emits: ["update:configuration"],
+	props: {
+		electricityTariff: { type: Object, required: true },
+	},
+	data() {
+		return {
+		};
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
