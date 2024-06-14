a86d6ba99fbc6934fe1d18a4362b8a7d4e311687
MartinRinas <martrin@microsoft.com>
2024-06-14 15:08:54 +0000

    remove config option, add deprecation warning

--- src/components/vehicles/psa/vehicle.vue
+++ src/components/vehicles/psa/vehicle.vue
@@ -1,96 +1,13 @@
 <template>
 	<div class="vehicle-soc-psa">
-		<openwb-base-text-input
-			title="Benutzername"
-			required
-			subtype="user"
-			:model-value="vehicle.configuration.user_id"
-			@update:model-value="
-				updateConfiguration($event, 'configuration.user_id')
-			"
-		>
-			<template #help>
-				Der Benutzername für die Anmeldung an den PSA-Servern.
-			</template>
-		</openwb-base-text-input>
-		<openwb-base-text-input
-			title="Kennwort"
-			required
-			subtype="password"
-			:model-value="vehicle.configuration.password"
-			@update:model-value="
-				updateConfiguration($event, 'configuration.password')
-			"
-		>
-			<template #help>
-				Das Passwort für die Anmeldung an den PSA-Servern.
-			</template>
-		</openwb-base-text-input>
-		<openwb-base-select-input
-			title="Marke"
-			required
-			notSelected="Bitte auswählen"
-			:options="[
-				{ value: 'Peugeot', text: 'Peugeot' },
-				{ value: 'Citroen', text: 'Citroen' },
-				{ value: 'DS', text: 'DS' },
-				{ value: 'Opel', text: 'Opel' },
-				{ value: 'Vauxhall', text: 'Vauxhall' },
-			]"
-			:model-value="vehicle.configuration.manufacturer"
-			@update:model-value="
-				updateConfiguration($event, 'configuration.manufacturer')
-			"
-		>
-			<template #help> Marke aus des Fahrzeugs.</template>
-		</openwb-base-select-input>
-
-		<openwb-base-text-input
-			title="VIN"
-			:model-value="vehicle.configuration.vin"
-			@update:model-value="
-				updateConfiguration($event, 'configuration.vin')
-			"
-		>
-			<template #help>
-				Die Fahrgestellnummer des Fahrzeugs. Nur notwendig falls mehrere
-				Fahrzeuge im selben Account vorhanden sind.
-			</template>
-		</openwb-base-text-input>
-		<openwb-base-card
-			title="Erweiterte Einstellungen"
-			subtype="info"
-			:collapsible="true"
-			:collapsed="true"
-		>
-			<openwb-base-text-input
-				title="Client ID"
-				subtype="user"
-				:model-value="vehicle.configuration.client_id"
-				@update:model-value="
-					updateConfiguration($event, 'configuration.client_id')
-				"
-			>
-				<template #help>
-					Die Client-ID für die Anmeldung an den PSA-Servern, muss
-					normalerweise nicht angegeben werden.
-				</template>
-			</openwb-base-text-input>
-
-			<openwb-base-text-input
-				title="Client Secret"
-				subtype="password"
-				:model-value="vehicle.configuration.client_secret"
-				@update:model-value="
-					updateConfiguration($event, 'configuration.client_secret')
-				"
-			>
-				<template #help>
-					Das Client-Secret für die Anmeldung an den PSA-Servern, muss
-					normalerweise nicht angegeben werden.
-				</template>
-			</openwb-base-text-input>
-		</openwb-base-card>
+		<openwb-base-alert subtype="danger">
+			Diese Modul ist seit Änderungen der Schnittstellen durch PSA 
+			nicht mehr funktionsfähig und wird in einer
+			zukünftigen Version entfernt. Bitte auf einen anderen Anbieter
+			wechseln, z.b. Tronity.<br>
+			Bei vorhandener PSA-Integration in Heimautomation kann der SoC
+			 ggf. auch per MQTT übergeben werden.
+		</openwb-base-alert>
 	</div>
 </template>
 
