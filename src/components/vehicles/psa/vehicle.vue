1bd4a6fe5a412e24dbd6573942ac62c1e26cd4c7
Martin Rinas <martrin@microsoft.com>
2024-06-17 13:01:19 +0200

    Update src/components/vehicles/psa/vehicle.vue
    Co-authored-by: LKuemmel <76958050+LKuemmel@users.noreply.github.com>

--- src/components/vehicles/psa/vehicle.vue
+++ src/components/vehicles/psa/vehicle.vue
@@ -4,7 +4,7 @@
 			Diese Modul ist seit Änderungen der Schnittstellen durch PSA 
 			nicht mehr funktionsfähig und wird in einer
 			zukünftigen Version entfernt. Bitte auf einen anderen Anbieter
-			wechseln, z.b. Tronity.<br>
+			wechseln, z.B. Tronity.<br>
 			Bei vorhandener PSA-Integration in Heimautomation kann der SoC
 			 ggf. auch per MQTT übergeben werden.
 		</openwb-base-alert>
