e6c453503b35649cc7515830d74839e9ebe9d1b1
LKuemmel <lena.kuemmel@openwb.de>
2024-07-02 13:22:45 +0200

    linting

--- src/components/devices/good_we/device.vue
+++ src/components/devices/good_we/device.vue
@@ -46,8 +46,8 @@
 			"
 		>
 			<template #help>
-				Die Wahl der API-Version beeinflusst die Auslesung der Speicherleistung 
-				und der Import/ Export Werte des Zählers.
+				Die Wahl der API-Version beeinflusst die Auslesung der
+				Speicherleistung und der Import/ Export Werte des Zählers.
 			</template>
 		</openwb-base-select-input>
 		<openwb-base-select-input
@@ -63,8 +63,8 @@
 			"
 		>
 			<template #help>
-				Die Auswahl der Firmware-Version beeinflusst die 
-				Auslesung der Wirkleistung und der Phasenströme des Zählers.
+				Die Auswahl der Firmware-Version beeinflusst die Auslesung der
+				Wirkleistung und der Phasenströme des Zählers.
 			</template>
 		</openwb-base-select-input>
 	</div>
