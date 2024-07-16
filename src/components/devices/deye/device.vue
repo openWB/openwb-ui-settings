860dc820abe30b828882f9d855b1f920553c6229
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2024-07-16 14:35:46 +0200

    add factor selection to UI (#503)
    * add factor selection to UI
    
    * adjust helptext
    
    * remove factor selection
    
    * remove selection of device_type

--- src/components/devices/deye/device.vue
+++ src/components/devices/deye/device.vue
@@ -22,35 +22,7 @@
 			@update:model-value="
 				updateConfiguration($event, 'configuration.port')
 			"
-		/>
-		<openwb-base-select-input
-			title="Bauform"
-			notSelected="Bitte auswählen"
-			:options="[
-				{
-					value: 'three_phase',
-					text: 'dreiphasiger Wechselrichter',
-				},
-				{
-					value: 'single_phase_string',
-					text: 'einphasiger String-Wechselrichter (ungetestet, Rückmeldung über Support erwünscht)',
-				},
-				{
-					value: 'single_phase_hybrid',
-					text: 'einphasiger Hybrid-Wechselrichter (ungetestet, Rückmeldung über Support erwünscht)',
-				},
-			]"
-			:model-value="configuration.device_type"
-			@update:model-value="
-				updateConfiguration($event, 'configuration.device_type')
-			"
-		>
-			<template #help>
-				Zwischen Version 1.7 und 1.10 hat sich das Register für die
-				Speicherleistung geändert. Wenn die Speicherleistung nicht
-				korrekt ausgelesen wird, bitte die andere Version testen.
-			</template>
-		</openwb-base-select-input>
+		/>		
 	</div>
 </template>
 
