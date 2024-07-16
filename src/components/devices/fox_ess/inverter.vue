5bcfd1cb6a50defc23a595ec535fa7767eade37e
ndrsnhs <andreas.neuhaus@openwb.de>
2024-07-16 14:21:53 +0200

    add fox_ess to UI

---
+++ src/components/devices/fox_ess/inverter.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="device-fox_ess-inverter">
+		<openwb-base-heading>
+			Einstellungen für FoxEss Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-number-input
+			title="Modbus ID"
+			required
+			:model-value="configuration.modbus_id"
+			min="1"
+			max="255"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.modbus_id')
+			"
+		/>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceFoxEssInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
