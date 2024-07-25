8a348d3aabf44bccad5e40abe156ddda373c39f8
ndrsnhs <andreas.neuhaus@openwb.de>
2024-07-25 12:54:56 +0200

    add varta inverter to UI

---
+++ src/components/devices/varta/inverter.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="device-varta-inverter">
+		<openwb-base-heading>
+			Einstellungen für Varta Wechselrichter
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-alert subtype="info">
+			Diese Komponente erfordert keine Einstellungen.
+		</openwb-base-alert>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceVartaInverter",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
