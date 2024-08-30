04d7e7a9b2b9f4c37587d5956cd525811a82333a
ndrsnhs <andreas.neuhaus@openwb.de>
2024-08-30 10:16:35 +0200

    add sigenergy to UI

---
+++ src/components/devices/sigenergy/bat.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="device-sigenergy-bat">
+		<openwb-base-heading>
+			Einstellungen für Sigenergy Batteriespeicher
+			<span class="small">(Modul: {{ $options.name }})</span>
+		</openwb-base-heading>
+		<openwb-base-number-input
+			title="Modbus ID"
+			required
+			:model-value="configuration.modbus_id"
+			min="1"
+			max="255"
+			@update:model-value="
+				updateConfiguration($event, 'configuration.modbus_id')
+			"
+		>
+			<template #help>
+				Die Standard-Modbus-ID von Sigenergy ist 247
+			</template>
+		</openwb-base-number-input>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "DeviceSigenergyBat",
+	emits: ["update:configuration"],
+	props: {
+		configuration: { type: Object, required: true },
+		deviceId: { default: undefined },
+		componentId: { required: true },
+	},
+	methods: {
+		updateConfiguration(event, path = undefined) {
+			this.$emit("update:configuration", { value: event, object: path });
+		},
+	},
+};
+</script>
