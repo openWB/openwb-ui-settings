f6ffe7204222e144ef4c303bbb3284bfc4c67504
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2024-10-10 10:48:38 +0200

    add infotext to inverter (#562)

---
+++ src/components/devices/varta/varta/inverter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="varta-inverter">
+    <openwb-base-alert subtype="info">
+      Die Leistung des Wechselrichters ist nur verfügbar, 
+      wenn dieser über Sensor an Varta angebunden ist.
+      Wechselrichter die per Modbus mit dem Speicher 
+      kommunizieren lassen sich nicht über den Varta 
+      PV-Sensor auslesen.
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceVartaInverter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
