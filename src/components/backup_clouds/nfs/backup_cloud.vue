0a24953f731a50ba128056a8d2b8c1093aeecf8d
LKuemmel <lena.kuemmel@openwb.de>
2024-11-21 15:54:13 +0100

    improve nfs help template

--- src/components/backup_clouds/nfs/backup_cloud.vue
+++ src/components/backup_clouds/nfs/backup_cloud.vue
@@ -6,7 +6,13 @@
       required
       :model-value="backupCloud.configuration.nfs_share"
       @update:model-value="updateConfiguration($event, 'configuration.nfs_share')"
-    />
+    >
+      <template #help>
+        1. IP basierter Pfad: 1.2.3.4:/pfad/zum/nfs-share<br />
+        2. Host (FQDN) basierter Pfad: backupserver.domain.net:/pfad/zum/nfs-share<br />
+        Bitte entgegen der Syntax das Protokoll nicht davor schreiben. Falsch wäre, “nfs://” davor zu setzen.
+      </template>
+    </openwb-base-text-input>
   </div>
 </template>
 
