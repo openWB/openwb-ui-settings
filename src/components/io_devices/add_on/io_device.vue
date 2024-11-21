f0d6ae786fb327adb052ff74258176c9f2ca01f7
Lutz Bender <github@lutz-bender.de>
2024-11-21 09:30:49 +0100

    io device configuration

---
+++ src/components/io_devices/add_on/io_device.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="io-device-add-on">
+    <OpenwbBaseAlert subtype="warning"> ToDo: Textfeld durch Select-Element ersetzen. </OpenwbBaseAlert>
+    <openwb-base-text-input
+      title="Installiert auf openWB"
+      subtype="host"
+      :model-value="ioDevice.configuration.host"
+      @update:model-value="updateState('configuration.host', $event)"
+    >
+      <template #help>
+        Bitte die openWB auswählen, auf der die Add-On-Platine installiert ist.<br />
+        Es werden nur Ladepunkte der Typen "interne openWB" und "externe openWB" unterstützt.
+      </template>
+    </openwb-base-text-input>
+  </div>
+</template>
+
+<script>
+import OpenwbIoDeviceConfigMixin from "../OpenwbIoDeviceConfigMixin.vue";
+
+export default {
+  name: "IoDeviceAddOn",
+  mixins: [OpenwbIoDeviceConfigMixin],
+};
+</script>
