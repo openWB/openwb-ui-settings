12be376cabb6fb275e0a5b35e3bae22030ac94f8
MartinRinas <martrin@microsoft.com>
2024-12-02 21:06:03 +0000

    countercard

--- src/components/status/CounterCard.vue
+++ src/components/status/CounterCard.vue
@@ -9,100 +9,127 @@
         fixed-width
         :icon="['fas', 'gauge-high']"
       />
-      {{ counter.name }} (ID: {{ counter.id }})
+      {{ counter.name }}
     </template>
-    <openwb-base-alert :subtype="statusLevel[$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state']]">
-      <font-awesome-icon
-        v-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state'] == 1"
-        fixed-width
-        :icon="['fas', 'exclamation-triangle']"
-      />
-      <font-awesome-icon
-        v-else-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state'] == 2"
-        fixed-width
-        :icon="['fas', 'times-circle']"
-      />
-      <font-awesome-icon
+    <template #actions>
+      <div v-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state'] == 0">
+        {{ formatNumberTopic("openWB/counter/" + counter.id + "/get/power", 3, 3, 0.001) }} kW
+      </div>
+      <openwb-base-label
         v-else
-        fixed-width
-        :icon="['fas', 'check-circle']"
+        subtype="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state']"
       />
-      Modulmeldung:<br />
-      <span style="white-space: pre-wrap">{{
-        $store.state.mqtt["openWB/counter/" + counter.id + "/get/fault_str"]
-      }}</span>
+    </template>
+    <openwb-base-alert subtype="light">
+      <table class="table table-sm table-borderless">
+        <tbody>
+          <tr>
+            <th>Aktuelle Werte</th>
+            <td class="text-right">Leistung</td>
+            <td class="text-right">Netzfrequenz</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="text-right text-monospace">
+              {{ formatNumberTopic("openWB/counter/" + counter.id + "/get/power", 3, 3, 0.001) + " kW" }}
+            </td>
+            <td class="text-right text-monospace">
+              {{ formatNumberTopic("openWB/counter/" + counter.id + "/get/frequency", 3) + " Hz" }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </openwb-base-alert>
-    <openwb-base-alert
-      v-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/state_str'] != undefined"
-      subtype="info"
-    >
-      Statusmeldung:<br />
-      <span style="white-space: pre-wrap">{{
-        $store.state.mqtt["openWB/counter/" + counter.id + "/get/state_str"]
-      }}</span>
+    <openwb-base-alert subtype="light">
+      <table class="table table-sm table-borderless">
+        <tbody>
+          <tr>
+            <th>Zählerstände</th>
+            <td class="text-right">Export</td>
+            <td class="text-right">Import</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td class="text-right text-monospace">
+              {{ formatNumberTopic("openWB/counter/" + counter.id + "/get/exported", 3, 3, 0.001) + " kWh" }}
+            </td>
+            <td class="text-right text-monospace">
+              {{ formatNumberTopic("openWB/counter/" + counter.id + "/get/imported", 3, 3, 0.001) + " kWh" }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </openwb-base-alert>
-    <openwb-base-heading>Zählerstände</openwb-base-heading>
-    <openwb-base-text-input
-      title="Export"
-      readonly
-      class="text-right text-monospace"
-      step="0.001"
-      unit="kWh"
-      :model-value="formatNumberTopic('openWB/counter/' + counter.id + '/get/exported', 3, 3, 0.001)"
-    />
-    <openwb-base-text-input
-      title="Import"
-      readonly
-      class="text-right text-monospace"
-      step="0.001"
-      unit="kWh"
-      :model-value="formatNumberTopic('openWB/counter/' + counter.id + '/get/imported', 3, 3, 0.001)"
-    />
-    <openwb-base-heading>Saldierte Werte</openwb-base-heading>
-    <openwb-base-text-input
-      title="Wirkleistung"
-      readonly
-      class="text-right text-monospace"
-      step="0.001"
-      unit="kW"
-      :model-value="formatNumberTopic('openWB/counter/' + counter.id + '/get/power', 3, 3, 0.001)"
-    />
-    <openwb-base-text-input
-      title="Netzfrequenz"
-      readonly
-      class="text-right text-monospace"
-      step="0.001"
-      unit="Hz"
-      :model-value="formatNumberTopic('openWB/counter/' + counter.id + '/get/frequency', 3)"
-    />
-    <openwb-base-heading>Werte pro Phase</openwb-base-heading>
-    <openwb-base-text-input
-      title="Spannung"
-      readonly
-      class="text-right text-monospace"
-      unit="V"
-      :model-value="formatPhaseArrayNumberTopic('openWB/counter/' + counter.id + '/get/voltages', 1)"
-    />
-    <openwb-base-text-input
-      title="Strom"
-      readonly
-      class="text-right text-monospace"
-      unit="A"
-      :model-value="formatPhaseArrayNumberTopic('openWB/counter/' + counter.id + '/get/currents', 2)"
-    />
-    <openwb-base-text-input
-      title="Wirkleistung"
-      readonly
-      class="text-right text-monospace"
-      unit="kW"
-      :model-value="formatPhaseArrayNumberTopic('openWB/counter/' + counter.id + '/get/powers', 3, 3, 0.001)"
-    />
-    <openwb-base-text-input
-      title="Leistungsfaktor"
-      readonly
-      class="text-right text-monospace"
-      :model-value="formatPhaseArrayNumberTopic('openWB/counter/' + counter.id + '/get/power_factors', 2)"
-    />
+    <openwb-base-alert subtype="light">
+      <table class="table table-sm table-borderless">
+        <tbody>
+          <tr>
+            <th colspan="3">Werte pro Phase</th>
+          </tr>
+          <tr>
+            <td class="text-right">⚡Spannung</td>
+            <td class="text-right text-monospace">
+              {{ formatPhaseArrayNumberTopic("openWB/counter/" + counter.id + "/get/voltages", 1) + " V" }}
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right">🔌Strom</td>
+            <td class="text-right text-monospace">
+              {{ formatPhaseArrayNumberTopic("openWB/counter/" + counter.id + "/get/currents", 2) + " A" }}
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right">Wirkleistung</td>
+            <td class="text-right text-monospace">
+              {{ formatPhaseArrayNumberTopic("openWB/counter/" + counter.id + "/get/powers", 3, 3, 0.001) + " kW" }}
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right">Leistungsfaktor</td>
+            <td class="text-right text-monospace">
+              {{ formatPhaseArrayNumberTopic("openWB/counter/" + counter.id + "/get/power_factors", 2) }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </openwb-base-alert>
+    <template #footer>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <openwb-base-alert
+              :subtype="statusLevel[$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state']]"
+            >
+              <font-awesome-icon
+                v-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state'] == 1"
+                fixed-width
+                :icon="['fas', 'exclamation-triangle']"
+              />
+              <font-awesome-icon
+                v-else-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state'] == 2"
+                fixed-width
+                :icon="['fas', 'times-circle']"
+              />
+              <font-awesome-icon
+                v-else
+                fixed-width
+                :icon="['fas', 'check-circle']"
+              />
+              Modulmeldung:
+              <span v-if="$store.state.mqtt['openWB/counter/' + counter.id + '/get/fault_state'] != 0">
+                <br />
+              </span>
+              <span style="white-space: pre-wrap">{{
+                $store.state.mqtt["'openWB/counter/' + counter.id + '/get//fault_str"]
+              }}</span>
+            </openwb-base-alert>
+          </div>
+          <div class="col col-auto">
+            <div class="text-right">ID: {{ counter.id }}</div>
+          </div>
+        </div>
+      </div>
+    </template>
   </openwb-base-card>
 </template>
 
