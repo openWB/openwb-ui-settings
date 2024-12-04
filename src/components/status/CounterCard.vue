50d43096e88f3876278d705cd70157b962f75d6d
MartinRinas <martrin@microsoft.com>
2024-12-04 07:40:36 +0000

    fix CounterCard

--- src/components/status/CounterCard.vue
+++ src/components/status/CounterCard.vue
@@ -75,21 +75,7 @@
               {{ formatPhaseArrayNumberTopic(baseTopic + "/get/voltages", 1).split(" / ")[1] + " V" }}
             </td>
             <td class="text-right text-monospace">
-              {{
-                formatPhaseArrayNumberTopic(baseTopic + "/get/voltages", 1)
-                  .split(" / ")[2]
-                  .split(" / ")[0] + " V"
-              }}
-            </td>
-            <td class="text-right text-monospace">
-              {{
-                formatPhaseArrayNumberTopic("openWB/counter/" + counter.id + "/get/voltages", 1).split(" / ")[1] + " V"
-              }}
-            </td>
-            <td class="text-right text-monospace">
-              {{
-                formatPhaseArrayNumberTopic("openWB/counter/" + counter.id + "/get/voltages", 1).split(" / ")[2] + " V"
-              }}
+              {{ formatPhaseArrayNumberTopic(baseTopic + "/get/voltages", 1).split(" / ")[0] + " V" }}
             </td>
           </tr>
           <tr>
