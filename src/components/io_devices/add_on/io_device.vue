9520e7805baff400ff3688016843ad7030b6a759
Lutz Bender <github@lutz-bender.de>
2024-12-09 14:00:04 +0100

    enhance modules

--- src/components/io_devices/add_on/io_device.vue
+++ src/components/io_devices/add_on/io_device.vue
@@ -1,25 +1,72 @@
 <template>
   <div class="io-device-add-on">
-    <OpenwbBaseAlert subtype="warning"> ToDo: Textfeld durch Select-Element ersetzen. </OpenwbBaseAlert>
-    <openwb-base-text-input
-      title="Installiert auf openWB"
-      subtype="host"
+    <openwb-base-select-input
+      title="Installiert auf..."
+      not-selected="Bitte auswählen"
+      :options="[{ value: 'localhost', text: 'dieser openWB' }]"
+      :groups="availableIoSources"
       :model-value="ioDevice.configuration.host"
-      @update:model-value="updateState('configuration.host', $event)"
+      @update:model-value="updateConfiguration($event, 'configuration.host')"
     >
       <template #help>
-        Bitte die openWB auswählen, auf der die Add-On-Platine installiert ist.<br />
-        Es werden nur Ladepunkte der Typen "interne openWB" und "externe openWB" unterstützt.
+        Bitte die Komponente auswählen, an der die Add-On-Platine installiert ist.<br />
+        Unterstützt werden:<br />
+        <ul>
+          <li>openWB standalone (als primary oder Zusatzdisplay)</li>
+          <li>openWB Ladepunkte als primary ("interne openWB") und secondary ("externe openWB")</li>
+        </ul>
       </template>
-    </openwb-base-text-input>
+    </openwb-base-select-input>
   </div>
 </template>
 
 <script>
 import OpenwbIoDeviceConfigMixin from "../OpenwbIoDeviceConfigMixin.vue";
+import ComponentState from "../../mixins/ComponentState.vue";
 
 export default {
   name: "IoDeviceAddOn",
-  mixins: [OpenwbIoDeviceConfigMixin],
+  mixins: [ComponentState, OpenwbIoDeviceConfigMixin],
+  data() {
+    return {
+      mqttTopicsToSubscribe: ["openWB/chargepoint/+/config"],
+    };
+  },
+  computed: {
+    configuredChargePoints() {
+      const chargePointConfigurations = this.getWildcardTopics("openWB/chargepoint/+/config");
+      let chargePoints = [];
+      for (const element of Object.values(chargePointConfigurations)) {
+        if (["external_openwb"].includes(element.type)) {
+          if (chargePoints.find((cp) => cp.value === element.configuration.ip_address)) {
+            continue;
+          }
+          chargePoints.push({
+            value: element.configuration.ip_address,
+            text: element.name,
+          });
+        }
+      }
+      return chargePoints;
+    },
+    configuredDisplays() {
+      // ToDo: Implement
+      return [];
+    },
+    availableIoSources: {
+      get() {
+        return [
+          {
+            label: "Ladepunkte",
+            options: this.configuredChargePoints,
+          },
+          {
+            label: "Zusatzdisplays",
+            options: this.configuredDisplays,
+          },
+        ].filter((group) => group.options.length > 0);
+      },
+    },
+  },
 };
 </script>
