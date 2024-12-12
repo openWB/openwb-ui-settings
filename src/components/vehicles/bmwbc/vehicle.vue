4bdc44d04fa9489831ec6f65362a510516db41d5
rleidner <89418596+rleidner@users.noreply.github.com>
2024-12-12 15:41:36 +0100

    soc_bmwbc: add captcha-token field required for initial login (#589)
    * soc_bmwbc: add captcha-token field required for initial login

--- src/components/vehicles/bmwbc/vehicle.vue
+++ src/components/vehicles/bmwbc/vehicle.vue
@@ -26,6 +26,34 @@
     >
       <template #help> Die Fahrgestellnummer des Fahrzeugs. </template>
     </openwb-base-text-input>
+    <openwb-base-text-input
+      title="Captcha-Token"
+      required
+      :model-value="vehicle.configuration.captcha_token"
+      @update:model-value="updateConfiguration($event, 'configuration.captcha_token')"
+    >
+      <template #help> Zum erstmaligen Login z.B. nach einem Neustart ist ein <b>aktuelles Captcha-Token</b> notwendig.<br>
+                       Dazu bitte folgende Schritte durchführen:<br>
+                       1. in einem neuen Browser-Tab auf diese Seite gehen:<br>
+                       <a
+                          href="https://bimmer-connected.readthedocs.io/en/latest/captcha/rest_of_world.html"
+                          target="_blank"
+                        >
+                        Captcha Lösen
+                        <font-awesome-icon
+                          fixed-width
+                          :icon="[fas, external-link-alt]"
+                        />
+                        </a>
+                          (https://bimmer-connected.readthedocs.io/en/latest/captcha/rest_of_world.html)<br>
+                       2. Dort falls angefragt das Captcha lösen und/oder "Ich bin ein Mensch" und dann Submit anclicken.<br>
+                          Als Ergebnis wird ein sehr langer String angezeigt. <br>
+                       3. Diesen String komplett mit <b>Copy&Paste</b> in das Feld Captcha-Token eingeben.<br>
+                       4. Die Konfiguration speichern.<br>
+                       5. <b>Sofort einmal im Desktop manuell (Kreispfeil) den SoC für das Fahrzeug abrufen.</b><br>
+                       6. <b>Fertig.</b> Ab jetzt wird bis zum nächsten Neustart das sog. Refresh-Token verwendet.<br>
+                       <b>Achtung: Das Captcha-Token kann nur einmal verwendet werden und gilt nur kurze Zeit!</b></template>
+    </openwb-base-text-input>
     <openwb-base-button-group-input
       title="SoC während der Ladung berechnen"
       :buttons="[
