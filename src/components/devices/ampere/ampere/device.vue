4ac13127f8b0f65aff32c6de792a15979cc5243f
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2024-12-17 11:44:26 +0100

    add ampere pro to UI (#587)

---
+++ src/components/devices/ampere/ampere/device.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="device-ampere">
+    <openwb-base-heading> Einstellungen für Ampere Pro</openwb-base-heading>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="
+        updateConfiguration($event, 'configuration.ip_address')
+      "
+    />
+    <openwb-base-number-input
+      title="Port"
+      required
+      :min="1"
+      :max="65535"
+      :model-value="device.configuration.port"
+      @update:model-value="
+        updateConfiguration($event, 'configuration.port')
+      "
+    />
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="device.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="
+        updateConfiguration($event, 'configuration.modbus_id')
+      "
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceAmpere",
+  mixins: [DeviceConfigMixin],
+};
+</script>
