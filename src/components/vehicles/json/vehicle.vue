ff178602ffd1270ae12c3cdd9f794f5287c5570a
MartinRinas <martrin@microsoft.com>
2024-12-17 08:13:32 +0000

    single URL, update help

--- src/components/vehicles/json/vehicle.vue
+++ src/components/vehicles/json/vehicle.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="vehicle-soc-json">
     <openwb-base-text-input
-      title="SoC URL"
+      title="URL"
       subtype="url"
       required
-      :model-value="vehicle.configuration.soc_url"
-      @update:model-value="updateConfiguration($event, 'configuration.soc_url')"
+      :model-value="vehicle.configuration.url"
+      @update:model-value="updateConfiguration($event, 'configuration.url')"
     >
       <template #help>
         Es wird eine komplette URL erwartet mit Angaben zum Protokoll, IP oder Hostnamen, optional einem Port und einem
@@ -20,23 +20,11 @@
       @update:model-value="updateConfiguration($event, 'configuration.soc_pattern')"
     >
       <template #help>
-        Zur Analyse der Werte aus dem json-Objekt wird jq benutzt. Ist die Json Antwort z.B.
-        {"response":{"value":"39.8","timestamp":1734054449}} So muss hier .response.value eingetragen werden.
+        Zur Analyse der Werte aus dem json-Objekt wird jq benutzt. Ist die Json Antwort z.B. {"response":{"soc":"39.8",
+        "range": "207", "timestamp":1734054449}} So muss hier .response.soc eingetragen werden.
         <br />
         Es wird vom Server eine Zahl mit oder ohne Nachkommastellen (Float, Integer) und einem Punkt als
         Dezimaltrennzeichen erwartet, welche den aktuellen Ladestand (SoC) darstellt.
-      </template>
-    </openwb-base-text-input>
-    <openwb-base-text-input
-      title="Reichweiten URL"
-      subtype="url"
-      :model-value="vehicle.configuration.range_url"
-      @update:model-value="updateConfiguration($event, 'configuration.range_url')"
-    >
-      <template #help>
-        Es wird eine komplette URL erwartet mit Angaben zum Protokoll, IP oder Hostnamen, optional einem Port und einem
-        Pfad. Die Antwort muss ein json-Objekt sein. Beispiel: http://192.168.1.1:8080/json?data=1. Diese Angabe ist
-        optional.
       </template>
     </openwb-base-text-input>
     <openwb-base-text-input
@@ -46,8 +34,8 @@
       @update:model-value="updateConfiguration($event, 'configuration.range_pattern')"
     >
       <template #help>
-        Zur Analyse der Werte aus dem json-Objekt wird jq benutzt. Ist die Json Antwort z.B.
-        {"response":{"value":"39.8","timestamp":1734054449}} So muss hier .response.value eingetragen werden.
+        Zur Analyse der Werte aus dem json-Objekt wird jq benutzt. Ist die Json Antwort z.B. {"response":{"soc":"39.8",
+        "range": "207", "timestamp":1734054449}} So muss hier .response.range eingetragen werden.
         <br />
         Es wird vom Server eine Zahl mit oder ohne Nachkommastellen (Float, Integer) und einem Punkt als
         Dezimaltrennzeichen erwartet, welche die aktuelle Reichweite darstellt.
