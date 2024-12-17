3422176f36f540f2f072dd6888a51120ec4dea4b
Lutz Bender <github@lutz-bender.de>
2024-12-17 09:06:20 +0100

    add io actions

---
+++ src/components/io_actions/OpenwbIoActionProxy.vue
@@ -0,0 +1,41 @@
+<template>
+  <openwb-base-heading> Einstellungen für I/O-Aktion "{{ ioAction.name }}" </openwb-base-heading>
+  <component
+    :is="myComponent"
+    :io-action="ioAction"
+    @update:configuration="updateConfiguration($event)"
+    @send-command="sendCommand($event)"
+  />
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+import OpenwbIoActionConfigFallback from "./OpenwbIoActionConfigFallback.vue";
+
+export default {
+  name: "OpenwbIoActionProxy",
+  props: {
+    ioAction: { type: Object, required: true },
+  },
+  emits: ["update:configuration", "sendCommand"],
+  computed: {
+    myComponent() {
+      console.debug(`loading io action: ${this.ioAction.type}`);
+      return defineAsyncComponent({
+        loader: () => import(`./${this.ioAction.type}/io_action.vue`),
+        errorComponent: OpenwbIoActionConfigFallback,
+      });
+    },
+  },
+  methods: {
+    // event pass through
+    updateConfiguration(event) {
+      this.$emit("update:configuration", event);
+    },
+    // event pass through
+    sendCommand(event) {
+      this.$emit("sendCommand", event);
+    },
+  },
+};
+</script>
