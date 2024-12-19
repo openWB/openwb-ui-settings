54f52fdba037c4e459b7d8157de27beb21563add
Lutz Bender <github@lutz-bender.de>
2024-12-19 12:58:39 +0100

    add consumption counter configuration

---
+++ src/components/devices/sonnen/sonnenbatterie/counter_consumption.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="device-sonnenbatterie-counter-consumption">
+    <openwb-base-alert subtype="info">
+      Diese Komponente liest die Daten des Verbrauchszählers der Sonnenbatterie aus, nicht die EVU-Daten! Es ist ggf.
+      ein virtueller Zähler als EVU-Zähler zu konfigurieren, falls kein anderer Zähler vorhanden ist und ausgelesen
+      werden kann.
+    </openwb-base-alert>
+    <openwb-base-alert subtype="warning">
+      Diese Komponente kann nur in Verbindung mit der JSON-API v2 verwendet werden, da die anderen Schnittstellen die
+      Daten nicht zur Verfügung stellen.
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceSonnenbatterieCounterConsumption",
+  mixins: [ComponentConfigMixin],
+};
+</script>
