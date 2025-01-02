4d14f14ae02e828c02960001d0b950044e2a09e3
ndrsnhs <andreas.neuhaus@openwb.de>
2025-01-02 14:07:23 +0100

    move max power into Chargepoint Card

--- src/components/status/ChargePointCard.vue
+++ src/components/status/ChargePointCard.vue
@@ -10,11 +10,7 @@
         fixed-width
         :icon="['fas', 'charging-station']"
       />
-      {{ installedChargePoint.name }} (Nennleistung: 
-      {{Math.floor((formatNumberTopic(
-          'openWB/chargepoint/' + chargePointIndex +
-          '/get/max_evse_current', 0)*3*230/1000))}}&nbsp;kW
-          )
+      {{ installedChargePoint.name }}
     </template>
     <template #actions>
       <div
@@ -58,6 +54,10 @@
         Statusmeldung:
         <span style="white-space: pre-wrap">{{ $store.state.mqtt[baseTopic + "/get/state_str"] }}</span>
       </openwb-base-alert>
+      max. Ladeleistung: 
+      {{(max_power = formatNumberTopic('openWB/chargepoint/' + chargePointIndex +
+              '/get/max_evse_current', 0)) == "-" ? max_power : Math.floor(max_power*3*230/1000)
+      }}&nbsp;kW
     </openwb-base-card>
 
     <openwb-base-card
