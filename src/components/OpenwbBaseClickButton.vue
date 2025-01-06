5c83e87a4bb5d8c318ac97acb033b2253d4387da
Lutz Bender <github@lutz-bender.de>
2025-01-06 13:40:48 +0100

    rse io action: add pattern handler

--- src/components/OpenwbBaseClickButton.vue
+++ src/components/OpenwbBaseClickButton.vue
@@ -2,6 +2,7 @@
   <button
     type="button"
     class="btn btn-block"
+    :title="tooltip"
     @click.prevent="click"
   >
     <slot>{{ title }}</slot>
@@ -13,6 +14,7 @@ export default {
   name: "OpenwbClickButton",
   props: {
     title: { type: String, required: false, default: "" },
+    tooltip: { type: String, required: false, default: "" },
   },
   emits: ["buttonClicked"],
   methods: {
---
+++ src/components/io_actions/controllable_consumers/ripple_control_receiver/io_pattern_config.vue
@@ -0,0 +1,207 @@
+<template>
+  <table class="w-100">
+    <colgroup>
+      <col
+        :span="numInputs"
+        class="bg-white"
+        style="border: 1px solid #dee2e6"
+      />
+    </colgroup>
+    <tr>
+      <th :colspan="numInputs">Eingangsmuster</th>
+      <th></th>
+      <th>
+        <openwb-base-click-button
+          class="bg-success text-white"
+          tooltip="Muster hinzufügen"
+          @click="addPattern()"
+        >
+          <font-awesome-icon :icon="['fas', 'plus']" />
+        </openwb-base-click-button>
+      </th>
+    </tr>
+    <tr>
+      <th
+        v-for="key in Object.keys(ioDevice.input.digital)"
+        :key="key"
+        class="input-header"
+      >
+        {{ key }}
+      </th>
+      <!-- ToDo: add analog inputs -->
+      <!-- <th
+        v-for="key in Object.keys(ioDevice.input.analog)"
+        :key="key"
+        class="input-header"
+      >
+        {{ key }}
+      </th> -->
+      <th>Verhalten</th>
+      <th></th>
+    </tr>
+    <tr
+      v-for="(pattern, patternKey) in value"
+      :key="patternKey"
+    >
+      <td
+        v-for="deviceInputKey in Object.keys(ioDevice.input.digital)"
+        :key="deviceInputKey"
+        class="input"
+      >
+        <font-awesome-icon
+          :title="getTitle(pattern.input_matrix[deviceInputKey])"
+          :icon="getIcon(pattern.input_matrix[deviceInputKey])"
+          :class="getIconClass(pattern.input_matrix[deviceInputKey])"
+          class="fa-fw clickable"
+          size="2x"
+          :transform="pattern.input_matrix[deviceInputKey] == undefined ? 'shrink-6' : null"
+          :mask="pattern.input_matrix[deviceInputKey] == undefined ? ['fas', 'square'] : null"
+          @click.stop.prevent="toggleInput(patternKey, deviceInputKey)"
+          @mousedown.stop.prevent
+          @mouseup.stop.prevent
+        />
+      </td>
+      <!-- ToDo: add analog inputs -->
+      <!-- <td
+        v-for="deviceInputKey in Object.keys(ioDevice.input.analog)"
+        :key="deviceInputKey"
+        class="input"
+      >
+        {{ deviceInputKey }}
+      </td> -->
+      <td>
+        <select
+          class="form-control"
+          required
+          :value="pattern.value"
+          @input="pattern.value = $event.target.value"
+        >
+          <option
+            disabled="disabled"
+            value="undefined"
+          >
+            -- Bitte auswählen --
+          </option>
+          <option value="0">sperren</option>
+          <option value="1">freigeben</option>
+        </select>
+      </td>
+      <td>
+        <openwb-base-click-button
+          class="bg-danger text-white"
+          tooltip="Muster löschen"
+          @click="deletePattern(patternKey)"
+        >
+          <font-awesome-icon :icon="['fas', 'trash']" />
+        </openwb-base-click-button>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSquare as fasSquare,
+  faQuestion as fasQuestion,
+  faTrash as fasTrash,
+  faPlus as fasPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { faSquare as farSquare, faSquareMinus as farSquareMinus } from "@fortawesome/free-regular-svg-icons";
+library.add(fasSquare, fasQuestion, farSquare, farSquareMinus, fasTrash, fasPlus);
+
+const states = {
+  undefined: {
+    value: undefined,
+    icon: ["fas", "question"],
+    iconClass: "text-secondary",
+    title: "Nicht relevant",
+    nextValue: true,
+  },
+  true: {
+    value: true,
+    icon: ["fas", "square"],
+    iconClass: "",
+    title: "Geschlossen",
+    nextValue: false,
+  },
+  false: {
+    value: false,
+    icon: ["far", "square"],
+    iconClass: "",
+    title: "Offen",
+    nextValue: undefined,
+  },
+};
+
+export default {
+  name: "IoPatternConfig",
+  components: {
+    FontAwesomeIcon,
+  },
+  inheritAttrs: false,
+  props: {
+    modelValue: { type: [Object, undefined], required: false, default: undefined },
+    ioDevice: { type: Object, required: true },
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(newValue) {
+        this.$emit("update:modelValue", newValue);
+      },
+    },
+    numInputs() {
+      return Object.keys(this.ioDevice.input.digital).length;
+      // ToDo: Add analog inputs
+      // return Object.keys(this.ioDevice.input.digital).length + Object.keys(this.ioDevice.input.analog).length;
+    },
+  },
+  methods: {
+    getIcon(input) {
+      return states[input].icon;
+    },
+    getIconClass(input) {
+      return states[input].iconClass;
+    },
+    getTitle(input) {
+      return states[input].title;
+    },
+    toggleInput(patternKey, deviceInputKey) {
+      const nextValue = states[this.value[patternKey].input_matrix[deviceInputKey]].nextValue;
+      if (nextValue !== undefined) {
+        this.value[patternKey].input_matrix[deviceInputKey] = nextValue;
+      } else {
+        delete this.value[patternKey].input_matrix[deviceInputKey];
+      }
+    },
+    deletePattern(patternKey) {
+      delete this.value.splice(patternKey, 1);
+    },
+    addPattern() {
+      this.value.push({
+        input_matrix: {},
+        value: 1,
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+th {
+  text-align: center;
+}
+
+.input-header {
+  transform: rotate(-90deg);
+}
+
+.input {
+  text-align: center;
+}
+</style>
