b9914166ca2bcf09735bc661a0a403c28917920c
Lutz Bender <github@lutz-bender.de>
2025-01-07 12:22:40 +0100

    rewrite rse action component

--- src/components/io_actions/controllable_consumers/ripple_control_receiver/io_action.vue
+++ src/components/io_actions/controllable_consumers/ripple_control_receiver/io_action.vue
@@ -1,43 +1,199 @@
 <template>
-  <div class="io-action-ripple-control-receiver">
-    <openwb-base-select-input
-      title="Eingangs-Gerät"
-      not-selected="Bitte auswählen"
-      required
-      :options="ioDeviceList"
-      :model-value="ioAction.configuration.io_device"
-      @update:model-value="updateConfiguration($event, 'configuration.io_device')"
-    >
-      <template #help>
-        Bitte das Gerät auswählen, über das der Schaltbefehl empfangen werden soll. Für jede Aktion kann nur ein Gerät
-        ausgewählt werden.
-      </template>
-    </openwb-base-select-input>
-    <hr />
-    <io-pattern-config
-      v-model="ioAction.configuration.input_pattern"
-      :io-device="ioDevices[`openWB/system/io/${ioAction.configuration.io_device}/config`]"
-    />
-    <hr />
-    Verhalten anwenden auf Ladepunkte...
-    {{ ioAction.configuration.cp_ids }}
-  </div>
+  <table class="w-100">
+    <colgroup>
+      <col
+        :span="numInputs"
+        class="bg-white"
+        style="border: 1px solid #dee2e6"
+      />
+    </colgroup>
+    <thead>
+      <tr>
+        <th :colspan="numInputs">Eingangsmuster</th>
+        <th colspan="2"></th>
+      </tr>
+      <tr>
+        <th
+          v-for="key in Object.keys(ioDevice.input.digital)"
+          :key="key"
+          class="input-header"
+        >
+          <div>{{ key }}</div>
+        </th>
+        <th>Verhalten</th>
+        <th>
+          <openwb-base-click-button
+            class="bg-success text-white"
+            tooltip="Muster hinzufügen"
+            @click="addPattern()"
+          >
+            <font-awesome-icon :icon="['fas', 'plus']" />
+          </openwb-base-click-button>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(pattern, patternKey) in value"
+        :key="patternKey"
+      >
+        <td
+          v-for="deviceInputKey in Object.keys(ioDevice.input.digital)"
+          :key="deviceInputKey"
+          class="input"
+        >
+          <font-awesome-icon
+            :title="getTitle(pattern.input_matrix[deviceInputKey])"
+            :icon="getIcon(pattern.input_matrix[deviceInputKey])"
+            :class="getIconClass(pattern.input_matrix[deviceInputKey])"
+            class="fa-fw clickable"
+            size="2x"
+            :transform="pattern.input_matrix[deviceInputKey] == undefined ? 'shrink-6' : null"
+            :mask="pattern.input_matrix[deviceInputKey] == undefined ? ['fas', 'square'] : null"
+            @click.stop.prevent="toggleInput(patternKey, deviceInputKey)"
+            @mousedown.stop.prevent
+            @mouseup.stop.prevent
+          />
+        </td>
+        <td>
+          <select
+            class="form-control"
+            required
+            :value="pattern.value"
+            @input="pattern.value = $event.target.value"
+          >
+            <option
+              disabled="disabled"
+              value="undefined"
+            >
+              -- Bitte auswählen --
+            </option>
+            <option value="0">sperren</option>
+            <option value="1">freigeben</option>
+          </select>
+        </td>
+        <td>
+          <openwb-base-click-button
+            class="bg-danger text-white"
+            tooltip="Muster löschen"
+            @click="deletePattern(patternKey)"
+          >
+            <font-awesome-icon :icon="['fas', 'trash']" />
+          </openwb-base-click-button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSquare as fasSquare,
+  faQuestion as fasQuestion,
+  faTrash as fasTrash,
+  faPlus as fasPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { faSquare as farSquare, faSquareMinus as farSquareMinus } from "@fortawesome/free-regular-svg-icons";
+library.add(fasSquare, fasQuestion, farSquare, farSquareMinus, fasTrash, fasPlus);
+
 import OpenwbIoActionConfigMixin from "../../OpenwbIoActionConfigMixin.vue";
-import IoPatternConfig from "./io_pattern_config.vue";
+
+const states = {
+  undefined: {
+    value: undefined,
+    icon: ["fas", "question"],
+    iconClass: "text-secondary",
+    title: "Nicht relevant",
+    nextValue: true,
+  },
+  true: {
+    value: true,
+    icon: ["fas", "square"],
+    iconClass: "",
+    title: "Geschlossen",
+    nextValue: false,
+  },
+  false: {
+    value: false,
+    icon: ["far", "square"],
+    iconClass: "",
+    title: "Offen",
+    nextValue: undefined,
+  },
+};
 
 export default {
   name: "IoActionRippleControlReceiver",
-  components: { IoPatternConfig },
+  components: {
+    FontAwesomeIcon,
+  },
   mixins: [OpenwbIoActionConfigMixin],
   computed: {
-    ioDeviceList() {
-      return Object.keys(this.ioDevices).map((key) => {
-        return { value: this.ioDevices[key].id, text: this.ioDevices[key].name };
+    value: {
+      get() {
+        return this.ioAction.configuration.input_pattern;
+      },
+      set(newValue) {
+        this.updateConfiguration(newValue, "configuration.input_pattern");
+      },
+    },
+    numInputs() {
+      return Object.keys(this.ioDevice.input.digital).length;
+    },
+  },
+  methods: {
+    getIcon(input) {
+      return states[input].icon;
+    },
+    getIconClass(input) {
+      return states[input].iconClass;
+    },
+    getTitle(input) {
+      return states[input].title;
+    },
+    toggleInput(patternKey, deviceInputKey) {
+      const nextValue = states[this.value[patternKey].input_matrix[deviceInputKey]].nextValue;
+      if (nextValue !== undefined) {
+        this.value[patternKey].input_matrix[deviceInputKey] = nextValue;
+      } else {
+        delete this.value[patternKey].input_matrix[deviceInputKey];
+      }
+    },
+    deletePattern(patternKey) {
+      delete this.value.splice(patternKey, 1);
+    },
+    addPattern() {
+      this.value.push({
+        input_matrix: {},
+        value: 1,
       });
     },
   },
 };
 </script>
+
+<style scoped>
+th {
+  text-align: center;
+  vertical-align: bottom;
+}
+
+.input-header {
+  position: relative;
+  height: 6em;
+}
+
+.input-header div {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: rotate(-90deg);
+  transform-origin: left center;
+}
+
+.input {
+  text-align: center;
+}
+</style>
