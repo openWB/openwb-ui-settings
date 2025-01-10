e0415360d7ea1c38e18d66701bedd3781e5e1f9d
Lutz Bender <github@lutz-bender.de>
2025-01-10 09:51:29 +0100

    unify rse and power_levels

--- src/components/io_actions/generator_systems/power_levels/io_action.vue
+++ src/components/io_actions/generator_systems/power_levels/io_action.vue
@@ -1,16 +1,55 @@
 <template>
-  <openwb-base-select-input
-    title="Eingang"
-    not-selected="Bitte auswählen"
-    :empty-value="null"
-    :options="inputOptions"
-    :model-value="ioAction?.configuration.digital_input"
-    @update:model-value="updateConfiguration($event, 'configuration.digital_input')"
-  >
-    <template #help>
-      Bitte den Eingang auswählen, auf welchen reagiert werden soll. Es kann nur ein Eingang ausgewählt werden.
-    </template>
-  </openwb-base-select-input>
+  <table class="w-100">
+    <colgroup>
+      <col
+        :span="numInputs"
+        class="bg-white"
+        style="border: 1px solid #dee2e6"
+      />
+    </colgroup>
+    <thead>
+      <tr>
+        <th :colspan="numInputs">Eingangsmuster</th>
+        <th colspan="2"></th>
+      </tr>
+      <tr>
+        <th
+          v-for="key in Object.keys(ioDevice.input.digital)"
+          :key="key"
+          class="input-header"
+        >
+          <div>{{ key }}</div>
+        </th>
+        <th>Erzeugungsleistung</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(pattern, patternKey) in value"
+        :key="patternKey"
+      >
+        <td
+          v-for="deviceInputKey in Object.keys(ioDevice.input.digital)"
+          :key="deviceInputKey"
+          class="text-center"
+        >
+          <font-awesome-icon
+            :title="getTitle(pattern.input_matrix[deviceInputKey])"
+            :icon="getIcon(pattern.input_matrix[deviceInputKey])"
+            :class="getIconClass(pattern.input_matrix[deviceInputKey])"
+            class="fa-fw clickable"
+            size="2x"
+            :transform="pattern.input_matrix[deviceInputKey] == undefined ? 'shrink-6' : null"
+            :mask="pattern.input_matrix[deviceInputKey] == undefined ? ['fas', 'square'] : null"
+            @click.stop.prevent="toggleInput(patternKey, deviceInputKey)"
+            @mousedown.stop.prevent
+            @mouseup.stop.prevent
+          />
+        </td>
+        <td class="text-center">{{ pattern.value * 100 }} %</td>
+      </tr>
+    </tbody>
+  </table>
   <hr />
   <openwb-base-heading title="Verhalten anwenden auf..."> Verhalten anwenden auf... </openwb-base-heading>
   <openwb-base-select-input
@@ -29,10 +68,48 @@
 </template>
 
 <script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSquare as fasSquare,
+  faQuestion as fasQuestion,
+  faTrash as fasTrash,
+  faPlus as fasPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { faSquare as farSquare, faSquareMinus as farSquareMinus } from "@fortawesome/free-regular-svg-icons";
+library.add(fasSquare, fasQuestion, farSquare, farSquareMinus, fasTrash, fasPlus);
+
 import OpenwbIoActionConfigMixin from "../../OpenwbIoActionConfigMixin.vue";
+
+const states = {
+  undefined: {
+    value: undefined,
+    icon: ["fas", "question"],
+    iconClass: "text-secondary",
+    title: "Nicht relevant",
+    nextValue: true,
+  },
+  true: {
+    value: true,
+    icon: ["fas", "square"],
+    iconClass: "",
+    title: "Geschlossen",
+    nextValue: false,
+  },
+  false: {
+    value: false,
+    icon: ["far", "square"],
+    iconClass: "",
+    title: "Offen",
+    nextValue: undefined,
+  },
+};
 
 export default {
   name: "IoActionPowerLevels",
+  components: {
+    FontAwesomeIcon,
+  },
   mixins: [OpenwbIoActionConfigMixin],
   computed: {
     value: {
@@ -43,10 +120,8 @@ export default {
         this.updateConfiguration(newValue, "configuration.input_pattern");
       },
     },
-    inputOptions() {
-      return Object.keys(this.ioDevice?.input?.digital).map((input) => {
-        return { value: input, text: input };
-      });
+    numInputs() {
+      return Object.keys(this.ioDevice.input.digital).length;
     },
     availableGenerators() {
       return this.availableComponents
@@ -56,5 +131,44 @@ export default {
         });
     },
   },
+  methods: {
+    getIcon(input) {
+      return states[input].icon;
+    },
+    getIconClass(input) {
+      return states[input].iconClass;
+    },
+    getTitle(input) {
+      return states[input].title;
+    },
+    toggleInput(patternKey, deviceInputKey) {
+      const nextValue = states[this.value[patternKey].input_matrix[deviceInputKey]].nextValue;
+      if (nextValue !== undefined) {
+        this.value[patternKey].input_matrix[deviceInputKey] = nextValue;
+      } else {
+        delete this.value[patternKey].input_matrix[deviceInputKey];
+      }
+    },
+  },
 };
 </script>
+
+<style scoped>
+th {
+  text-align: center;
+  vertical-align: bottom;
+}
+
+.input-header {
+  position: relative;
+  height: 6em;
+}
+
+.input-header div {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: rotate(-90deg);
+  transform-origin: left center;
+}
+</style>
