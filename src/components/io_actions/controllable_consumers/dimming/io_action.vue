a849d3d31247aaac602adc0f45d65af8eceb91e0
Lutz Bender <github@lutz-bender.de>
2025-01-16 16:22:35 +0100

    simplify io actions with only one input

--- src/components/io_actions/controllable_consumers/dimming/io_action.vue
+++ src/components/io_actions/controllable_consumers/dimming/io_action.vue
@@ -1,17 +1,31 @@
 <template>
   <openwb-base-select-input
+    v-model="definedInput"
     title="Eingang"
     not-selected="Bitte auswählen"
     :empty-value="null"
     :options="inputOptions"
-    :model-value="ioAction?.configuration.digital_input"
-    @update:model-value="updateConfiguration($event, 'configuration.digital_input')"
   >
     <template #help>
       Bitte den Eingang auswählen, auf welchen reagiert werden soll. Es kann nur ein Eingang ausgewählt werden.
     </template>
   </openwb-base-select-input>
+  <openwb-base-button-group-input
+    v-if="definedInput"
+    v-model="definedNormalMode"
+    title="Normaler Zustand"
+    :buttons="[
+      { buttonValue: true, text: 'geschlossen (NC)' },
+      { buttonValue: false, text: 'geöffnet (NO)' },
+    ]"
+  >
+    <template #help>
+      Bitte Einstellen, ob der Eingang im normalen Zustand geschlossen (NC) oder geöffnet (NO) ist. Dieser Zustand wird
+      verwendet, um den Eingang zu überwachen.
+    </template>
+  </openwb-base-button-group-input>
   <openwb-base-number-input
+    v-if="definedInput"
     title="maximale Bezugsleistung"
     unit="kW"
     min="0"
@@ -21,17 +35,17 @@
   >
     <template #help> Bitte die maximale Bezugsleistung in Watt angeben. </template>
   </openwb-base-number-input>
-  <openwb-base-heading>Anwenden auf...</openwb-base-heading>
+  <hr />
   <openwb-base-select-input
-    title="Ladepunkte"
-    :options="availableChargePoints"
-    :model-value="ioAction?.configuration.cp_ids"
+    title="Anwenden auf..."
+    :groups="availableDevices"
+    :model-value="ioAction?.configuration.devices"
     multiple
-    @update:model-value="updateConfiguration($event, 'configuration.cp_ids')"
+    @update:model-value="updateConfiguration($event, 'configuration.devices')"
   >
     <template #help>
-      Bitte die Ladepunkte auswählen, welche berücksichtigt werden sollen. Es können mehrere Ladepunkte ausgewählt
-      werden.
+      Bitte die Ladepunkte und/oder Komponenten auswählen, welche berücksichtigt werden sollen. Es können mehrere
+      Einträge ausgewählt werden.
     </template>
   </openwb-base-select-input>
 </template>
@@ -40,7 +54,7 @@
 import OpenwbIoActionConfigMixin from "../../OpenwbIoActionConfigMixin.vue";
 
 export default {
-  name: "IoActionDimmingDirectControl",
+  name: "IoActionDimming",
   mixins: [OpenwbIoActionConfigMixin],
   computed: {
     value: {
@@ -51,10 +65,50 @@ export default {
         this.updateConfiguration(newValue, "configuration.input_pattern");
       },
     },
+    definedInput: {
+      get() {
+        if (Object.keys(this.value[0].input_matrix).length !== 0) {
+          return Object.keys(this.value[0].input_matrix)[0];
+        }
+        return null;
+      },
+      set(newValue) {
+        if (Object.keys(this.value[0].input_matrix).length === 0) {
+          this.value = [
+            { value: true, input_matrix: { [newValue]: true } },
+            { value: false, input_matrix: { [newValue]: false } },
+          ];
+          return;
+        }
+        this.value = [
+          { value: true, input_matrix: { [newValue]: this.definedNormalMode } },
+          { value: false, input_matrix: { [newValue]: !this.definedNormalMode } },
+        ];
+      },
+    },
+    definedNormalMode: {
+      get() {
+        return Object.values(this.value[0].input_matrix)[0];
+      },
+      set(newValue) {
+        this.value = [
+          { value: true, input_matrix: { [this.definedInput]: newValue } },
+          { value: false, input_matrix: { [this.definedInput]: !newValue } },
+        ];
+      },
+    },
     inputOptions() {
       return Object.keys(this.ioDevice?.input?.digital).map((input) => {
         return { value: input, text: input };
       });
+    },
+    availableDevices() {
+      return [
+        {
+          label: "Ladepunkte",
+          options: this.availableChargePoints.map((cp) => ({ value: `cp${cp.value}`, text: cp.text })),
+        },
+      ];
     },
   },
 };
