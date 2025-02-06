ed78bd389468738430b32a591126e0d758f8be1d
ndrsnhs <andreas.neuhaus@openwb.de>
2025-02-06 13:16:06 +0100

    add div

--- src/components/status/ChargePointCard.vue
+++ src/components/status/ChargePointCard.vue
@@ -54,12 +54,14 @@
         Statusmeldung:
         <span style="white-space: pre-wrap">{{ $store.state.mqtt[baseTopic + "/get/state_str"] }}</span>
       </openwb-base-alert>
-      max. Ladeleistung:
-      {{
-        (max_power = formatNumberTopic("openWB/chargepoint/" + chargePointIndex + "/get/max_evse_current", 0)) == "-"
-          ? max_power
-          : Math.floor((max_power * 3 * 230) / 1000)
-      }}&nbsp;kW
+      <div>
+        max. Ladeleistung:
+        {{
+          (max_power = formatNumberTopic("openWB/chargepoint/" + chargePointIndex + "/get/max_evse_current", 0)) == "-"
+            ? max_power
+            : Math.floor((max_power * 3 * 230) / 1000)
+        }}&nbsp;kW
+      </div>
     </openwb-base-card>
 
     <openwb-base-card
