18f6d91738b06c54a9924a4a2686633425b31a5d
ddraeyer <53512661+ddraeyer@users.noreply.github.com>
2025-03-13 20:47:50 +0100

    vehicle.vue for Leaf
    vehicle.vue for Leaf added

---
+++ src/components/vehicles/leaf/vehicle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="vehicle-soc-leaf">
+    <openwb-base-text-input
+      title="Benutzername"
+      required
+      subtype="user"
+      :model-value="vehicle.configuration.user_id"
+      @update:model-value="updateConfiguration($event, 'configuration.user_id')"
+    >
+      <template #help> Der Benutzername für die Anmeldung an den Nissan-Servern. </template>
+    </openwb-base-text-input>
+    <openwb-base-text-input
+      title="Kennwort"
+      required
+      subtype="password"
+      :model-value="vehicle.configuration.password"
+      @update:model-value="updateConfiguration($event, 'configuration.password')"
+    >
+      <template #help> Das Passwort für die Anmeldung an den Nissan-Servern. </template>
+    </openwb-base-text-input>
+  </div>
+</template>
+
+<script>
+import VehicleConfigMixin from "../VehicleConfigMixin.vue";
+
+export default {
+  name: "VehicleSocLeaf",
+  mixins: [VehicleConfigMixin],
+};
+</script>
