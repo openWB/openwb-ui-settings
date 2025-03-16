205b3c78d9755e7f99abe75818587e7ba96bd4e2
ddraeyer <53512661+ddraeyer@users.noreply.github.com>
2025-03-16 09:57:00 +0100

    Extended with "region"
    Extended with the additional configuration parameter "region"

--- src/components/vehicles/leaf/vehicle.vue
+++ src/components/vehicles/leaf/vehicle.vue
@@ -18,6 +18,24 @@
     >
       <template #help> Das Passwort für die Anmeldung an den Nissan-Servern. </template>
     </openwb-base-text-input>
+	<openwb-base-text-input
+      title="Region"
+      required
+      :model-value="vehicle.configuration.region"
+      @update:model-value="updateConfiguration($event, 'configuration.region')"
+    >
+      <template #help>
+		Das Regionskürzel für die Anmeldung an den Nissan-Servern. z.B. NE für Europa.<br />
+		Liste der Regionskürzel:<br />
+		<ol>
+			<li>NE : Europa, getestet,</li>
+			<li>NNA : USA, nicht getestet,</li>
+			<li>NCI : Kanada, nicht getestet,</li>
+			<li>NMA : Australien, nicht getestet,</li>
+			<li>NML : Japan, nicht getestet.</li>
+		</ol>
+	  </template>
+    </openwb-base-text-input>
   </div>
 </template>
 
