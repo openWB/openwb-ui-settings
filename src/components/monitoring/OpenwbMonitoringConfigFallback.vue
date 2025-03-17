e080c6bf18d678e19271fda63f9e727b4598a305
Lutz Bender <github@lutz-bender.de>
2025-03-17 09:11:22 +0100

    show official badge for backup-cloud, electricity tariff, monitoring, vehicles modules

--- src/components/monitoring/OpenwbMonitoringConfigFallback.vue
+++ src/components/monitoring/OpenwbMonitoringConfigFallback.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="monitoring-fallback">
-    <openwb-base-alert subtype="warning">
-      Es wurde keine Konfigurationsseite für den Anbieter "{{ monitoring.name }}" gefunden. Die Einstellungen können als
-      JSON direkt bearbeitet werden.
-    </openwb-base-alert>
-    <openwb-base-textarea
-      title="Konfiguration"
-      subtype="json"
-      :model-value="monitoring.configuration"
-      @update:model-value="updateConfiguration($event, 'configuration')"
+    <openwb-base-alert
+      v-if="Object.keys(monitoring.configuration).length == 0"
+      subtype="info"
     >
-      <template #help> Bitte prüfen Sie, ob die Eingaben richtig interpretiert werden. </template>
-    </openwb-base-textarea>
-    <openwb-base-alert subtype="info">
-      <pre>{{ JSON.stringify(monitoring.configuration, undefined, 2) }}</pre>
+      Der Anbieter "{{ monitoring.name }}" bietet keine Einstellungen.
     </openwb-base-alert>
+    <div v-else>
+      <openwb-base-alert subtype="warning">
+        Es wurde keine Konfigurationsseite für den Anbieter "{{ monitoring.name }}" gefunden. Die Einstellungen können
+        als JSON direkt bearbeitet werden.
+      </openwb-base-alert>
+      <openwb-base-textarea
+        title="Konfiguration"
+        subtype="json"
+        :model-value="monitoring.configuration"
+        @update:model-value="updateConfiguration($event, 'configuration')"
+      >
+        <template #help> Bitte prüfen Sie, ob die Eingaben richtig interpretiert werden. </template>
+      </openwb-base-textarea>
+      <openwb-base-alert subtype="info">
+        <pre>{{ JSON.stringify(monitoring.configuration, undefined, 2) }}</pre>
+      </openwb-base-alert>
+    </div>
   </div>
 </template>
 
