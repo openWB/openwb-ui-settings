784f2ebdc86b6c0b02a1b38a94ff90cecc397e56
Brett-S-OWB <brett.scott@openwb.de>
2025-04-07 16:45:14 +0200

    Add Koala theme config - charge point view breakpoint and search input visibility

--- src/components/web_themes/koala/webTheme.vue
+++ src/components/web_themes/koala/webTheme.vue
@@ -13,6 +13,39 @@
         Das Zeitfenster für das Verlaufsdiagramm wird auf die hier eingestellte Anzahl Minuten beschränkt.
       </template>
     </openwb-base-range-input>
+    <openwb-base-number-input
+      title="Kartenansicht Grenzwert"
+      :min="1"
+      :step="1"
+      :model-value="webTheme.configuration.card_view_breakpoint"
+      @update:model-value="updateConfiguration($event, 'configuration.card_view_breakpoint')"
+    >
+      <template #help>
+        Legt fest, bis zu wie vielen Ladepunkten die Kartenansicht verwendet wird. Bei mehr Ladepunkten wird automatisch
+        zur Tabellenansicht gewechselt.
+      </template>
+    </openwb-base-number-input>
+    <openwb-base-button-group-input
+      title="Suchfeld in Ladepunkt Tabellenansicht"
+      :buttons="[
+        {
+          buttonValue: false,
+          text: 'Nein',
+          class: 'btn-outline-danger',
+        },
+        {
+          buttonValue: true,
+          text: 'Ja',
+          class: 'btn-outline-success',
+        },
+      ]"
+      :model-value="webTheme.configuration.table_search_input_field"
+      @update:model-value="updateConfiguration($event, 'configuration.table_search_input_field')"
+    >
+      <template #help>
+        Legt fest, ob in der Tabellenansicht ein Suchfeld angezeigt wird, mit dem nach Ladepunkten gesucht werden kann.
+      </template>
+    </openwb-base-button-group-input>
   </div>
 </template>
 
