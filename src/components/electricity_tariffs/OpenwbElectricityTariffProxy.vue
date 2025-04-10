eab6e6b57c2ddd662a0d5497c51253173c8fb982
benderl <benderl@users.noreply.github.com>
2025-04-10 14:58:46 +0200

    Feature official modules (#692)
    * show official badge for backup-cloud, electricity tariff, monitoring, vehicles modules
    
    * hide missing commands badge for charge points

--- src/components/electricity_tariffs/OpenwbElectricityTariffProxy.vue
+++ src/components/electricity_tariffs/OpenwbElectricityTariffProxy.vue
@@ -1,4 +1,25 @@
 <template>
+  <openwb-base-alert
+    v-if="electricityTariff.official"
+    subtype="success"
+  >
+    <font-awesome-icon
+      fixed-width
+      :icon="['fas', 'certificate']"
+    />
+    Das ausgewählte Modul "{{ electricityTariff.name }}" wird von openWB gepflegt.
+  </openwb-base-alert>
+  <openwb-base-alert
+    v-else
+    subtype="info"
+  >
+    <font-awesome-icon
+      fixed-width
+      :icon="['fas', 'people-group']"
+    />
+    Das ausgewählte Modul "{{ electricityTariff.name }}" wird in unserer Community gepflegt. Rückfragen oder Probleme
+    bitte im Forum diskutieren.
+  </openwb-base-alert>
   <openwb-base-heading> Einstellungen für Modul "{{ electricityTariff.name }}" </openwb-base-heading>
   <component
     :is="getTariffComponent()"
@@ -8,11 +29,20 @@
 </template>
 
 <script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPeopleGroup as fasPeopleGroup, faCertificate as fasCertificate } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fasPeopleGroup, fasCertificate);
+
 import { defineAsyncComponent } from "vue";
 import OpenwbElectricityTariffFallback from "./OpenwbElectricityTariffConfigFallback.vue";
 
 export default {
   name: "OpenwbElectricityTariffProxy",
+  components: {
+    FontAwesomeIcon,
+  },
   props: {
     electricityTariff: { type: Object, required: true },
   },
