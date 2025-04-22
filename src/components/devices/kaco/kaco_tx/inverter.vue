dafb6ae2f9d3045662509c10486057c464f8e13c
ndrsnhs <andreas.neuhaus@openwb.de>
2025-04-22 10:09:17 +0200

    add Kaco Tx Inverter

---
+++ src/components/devices/kaco/kaco_tx/inverter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="device-kaco-inverter">
+    <openwb-base-heading> Einstellungen für Kaco Wechselrichter </openwb-base-heading>
+    <openwb-base-number-input
+      title="Kaco-Modbus-ID"
+      required
+      :model-value="component.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceKacoInverter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
