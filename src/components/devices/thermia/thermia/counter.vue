69128b26362c5cb962780fa4ee844091672696bb
cr0i <christoph@rischke.net>
2025-05-06 07:59:27 +0200

    Update (#707)

---
+++ src/components/devices/thermia/thermia/counter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="device-thermia-counter">
+    <openwb-base-alert subtype="info"> ModbusTCP muss aktiviert sein. </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceThermiaCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
