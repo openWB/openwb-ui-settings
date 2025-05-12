8c037f1815a398503ce68bb1a7aebf5e655d8f46
Lutz Bender <github@lutz-bender.de>
2025-05-12 12:56:10 +0200

    add currents to json inverter

--- src/components/devices/generic/json/inverter.vue
+++ src/components/devices/generic/json/inverter.vue
@@ -27,6 +27,25 @@
     >
       <template #help> Wird dieses Feld leer gelassen, dann werden Zählerstände intern simuliert. </template>
     </openwb-base-text-input>
+
+    <openwb-base-text-input
+      title="Abfrage für Strom auf Phase 1"
+      subtype="text"
+      :model-value="component.configuration.jq_current_l1"
+      @update:model-value="updateConfiguration($event, 'configuration.jq_current_l1')"
+    />
+    <openwb-base-text-input
+      title="Abfrage für Strom auf Phase 2"
+      subtype="text"
+      :model-value="component.configuration.jq_current_l2"
+      @update:model-value="updateConfiguration($event, 'configuration.jq_current_l2')"
+    />
+    <openwb-base-text-input
+      title="Abfrage für Strom auf Phase 3"
+      subtype="text"
+      :model-value="component.configuration.jq_current_l3"
+      @update:model-value="updateConfiguration($event, 'configuration.jq_current_l3')"
+    />
   </div>
 </template>
 
