66f3fe1be99ebf003faa65dd2a85c3ae5b32e690
LKuemmel <lena.kuemmel@openwb.de>
2025-05-27 11:54:34 +0200

    new topics for mqtt vehicle

--- src/components/vehicles/mqtt/vehicle.vue
+++ src/components/vehicles/mqtt/vehicle.vue
@@ -9,7 +9,7 @@
             class="text-info"
             tooltip="Topic kopieren"
           >
-            openWB/set/vehicle/{{ vehicleId }}/get/soc
+            openWB/set/mqtt/vehicle/{{ vehicleId }}/get/soc
           </openwb-base-copy-to-clipboard>
           <br />
           Fahrzeug-SoC mit Nachkommastellen (Float) oder Ganzzahl<br />
@@ -20,7 +20,20 @@
             class="text-info"
             tooltip="Topic kopieren"
           >
-            openWB/set/vehicle/{{ vehicleId }}/get/range
+            openWB/set/mqtt/vehicle/{{ vehicleId }}/get/soc_timestamp
+          </openwb-base-copy-to-clipboard>
+          <br />
+          Zeitstempel des SoCs in s als Unix-Zeitstempel<br />
+          Diese Info ist optional. Wird kein Wert für das Topic veröffentlicht, wird bei der Abfrage automatisch der
+          aktuelle Zeitstempel gesetzt.<br />
+          Beispiel: <span class="text-info">1748339524</span>
+        </li>
+        <li>
+          <openwb-base-copy-to-clipboard
+            class="text-info"
+            tooltip="Topic kopieren"
+          >
+            openWB/set/mqtt/vehicle/{{ vehicleId }}/get/range
           </openwb-base-copy-to-clipboard>
           <br />
           Reichweite des Fahrzeugs in km mit Nachkommastellen (Float) oder Ganzzahl<br />
