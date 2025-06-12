56fb9a048159a4a33164bc30a71fa6e634c64977
Lutz Bender <github@lutz-bender.de>
2025-06-12 09:19:30 +0200

    fix charge point default color

--- src/views/ChargePointInstallation.vue
+++ src/views/ChargePointInstallation.vue
@@ -72,7 +72,7 @@
               <openwb-base-color-picker
                 class="p-1"
                 :model-value="installedChargePoint.color"
-                default-color="#17a2b8"
+                default-color="#0000ff"
                 @update:model-value="updateState(installedChargePointKey, $event, 'color')"
               />
             </template>
