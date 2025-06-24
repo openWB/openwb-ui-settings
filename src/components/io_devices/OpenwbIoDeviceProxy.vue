d8a0df98758fa557c224650dc231a741c017309d
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2025-06-24 10:06:09 +0200

    Set output manual (#731)
    * draft
    
    * fixes
    
    * typo
    
    * improve code

--- src/components/io_devices/OpenwbIoDeviceProxy.vue
+++ src/components/io_devices/OpenwbIoDeviceProxy.vue
@@ -1,11 +1,36 @@
 <template>
-  <openwb-base-heading> Einstellungen für I/O-Modul "{{ ioDevice.name }}" </openwb-base-heading>
-  <component
-    :is="myComponent"
-    :io-device="ioDevice"
-    @update:configuration="updateConfiguration($event)"
-    @send-command="sendCommand($event)"
-  />
+  <div>
+    <openwb-base-heading> Einstellungen für I/O-Modul "{{ ioDevice.name }}" </openwb-base-heading>
+    <openwb-base-alert
+      v-if="outputs.length"
+      subtype="info"
+    >
+      Die Ausgänge können über die folgenden Topics manuell per MQTT geschaltet werden. Falls Ausgänge in Aktionen
+      konfiguriert werden, überschreibt das manuelle Setzen den Status.
+      <ul>
+        <li
+          v-for="output in outputs"
+          :key="output"
+        >
+          <openwb-base-copy-to-clipboard
+            class="text-info"
+            tooltip="Topic kopieren"
+          >
+            {{ output }}
+          </openwb-base-copy-to-clipboard>
+        </li>
+        Als Wert wird ein boolean als json oder 0/1 akzeptiert.<br />
+        Beispiel:
+        <span class="text-info">false</span>
+      </ul>
+    </openwb-base-alert>
+    <component
+      :is="myComponent"
+      :io-device="ioDevice"
+      @update:configuration="updateConfiguration($event)"
+      @send-command="sendCommand($event)"
+    />
+  </div>
 </template>
 
 <script>
@@ -25,6 +50,22 @@ export default {
         loader: () => import(`./${this.ioDevice.type}/io_device.vue`),
         errorComponent: OpenwbIoDeviceConfigFallback,
       });
+    },
+    outputs() {
+      const outputs = [];
+      if (this.ioDevice.output) {
+        if (this.ioDevice.output.analog) {
+          for (const key in this.ioDevice.output.analog) {
+            outputs.push(`openWB/set/system/io/${this.ioDevice.id}/set/manual/analog_output/${key}`);
+          }
+        }
+        if (this.ioDevice.output.digital) {
+          for (const key in this.ioDevice.output.digital) {
+            outputs.push(`openWB/set/system/io/${this.ioDevice.id}/set/manual/digital_output/${key}`);
+          }
+        }
+      }
+      return outputs;
     },
   },
   methods: {
