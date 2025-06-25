c94a0b74eb6c8b67ae575ff090dddd0258cee382
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2025-06-25 12:54:43 +0200

    Smarthome devices as counters (#711)
    * mystrom, orno
    
    * avm
    
    * nibe

---
+++ src/components/devices/orno/orno/counter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="device-orno-counter">
+    <openwb-base-heading> Einstellungen für Orno WE-514 Zähler </openwb-base-heading>
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="component.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceOrnoCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
