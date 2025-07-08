a7c1bab6bbb6fc8fac12395afb435d69fa6f59b2
ndrsnhs <andreas.neuhaus@openwb.de>
2025-07-08 14:46:49 +0200

    add battery_index

---
+++ src/components/devices/good_we/good_we/bat.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="device-goodwe-bat">
+    <openwb-base-heading> Einstellungen für GoodWe Batteriespeicher </openwb-base-heading>
+    <openwb-base-number-input
+      title="GoodWe-Batterie-Index"
+      :model-value="component.configuration.battery_index"
+      min="1"
+      max="2"
+      @update:model-value="updateConfiguration($event, 'configuration.battery_index')"
+    >
+      <template #help>
+        Wenn eine zweite Batterie am Wechselrichter angeschlossen ist, hier den Index 2 eintagen. Im Normalfall immer 1
+        eintragen.
+      </template>
+    </openwb-base-number-input>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceGoodWeBat",
+  mixins: [ComponentConfigMixin],
+};
+</script>
