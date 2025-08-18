d9191bac2e7e70edd70edb77ebee3d10ab320641
ndrsnhs <andreas.neuhaus@openwb.de>
2025-08-18 14:22:14 +0200

    add kostal piko ci

---
+++ src/components/devices/kostal/kostal_piko_ci/counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="device-kostal-piko-ci-counter">
+    <openwb-base-heading> Einstellungen für Kostal Piko CI Zähler </openwb-base-heading>
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="component.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    >
+      <template #help> Die Standard-Modbus-ID von Kostal Piko CI ist 75 </template>
+    </openwb-base-number-input>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceKostalPikoCiCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
