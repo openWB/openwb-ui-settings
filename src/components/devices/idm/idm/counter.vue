b02892027450c8b9c70f45d43f890aeb5d0b1ba4
SeaSpotter <florian.horch@gmail.com>
2025-09-17 07:45:15 +0200

    Create counter.vue

---
+++ src/components/devices/idm/idm/counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="device-idm-counter">
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceIDMCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
