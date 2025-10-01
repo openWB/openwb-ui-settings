da7c03224965e41df92969183cd0d43caea02eb0
Lutz Bender <github@lutz-bender.de>
2025-10-01 08:18:05 +0200

    allow upload of gpg files

--- src/views/DataManagement.vue
+++ src/views/DataManagement.vue
@@ -87,7 +87,7 @@
                 id="input-file"
                 type="file"
                 class="custom-file-input"
-                accept=".tar.gz,application/gzip,application/tar+gzip"
+                accept=".tar.gz,application/gzip,application/tar+gzip,.tar.gz.gpg,application/gzip+gpg,application/tar+gzip+gpg"
                 @change="updateSelectedRestoreFile($event)"
               />
               <label
