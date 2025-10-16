7acf11699a4c244eb42ec7c09d32b4e158789853
LKuemmel <lena.kuemmel@openwb.de>
2025-10-16 16:28:19 +0200

    update bmw help

--- src/components/vehicles/bmwbc/vehicle.vue
+++ src/components/vehicles/bmwbc/vehicle.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="vehicle-soc-bmwbc">
     <openwb-base-alert subtype="info">
-      Beschreibung der Konfiguration und weitere Hinweise im Wiki:
-      <a
-        href="https://github.com/openWB/core/wiki/SoC-BMW-Mini"
-        target="_blank"
-        rel="noopener noreferrer"
-        >Wiki für SoC-Modul BMW & Mini</a
-      >
+      Zum erstmaligen Login z.B. nach einem Neustart oder wenn die Abfrage nicht mehr funktioniert ist ein
+      <b>aktuelles Captcha-Token</b> notwendig.<br />
+      Falls mehr als ein Fahrzeug in dem CD-Account vorhanden ist, sollte nur das mit der niedrigsten openWB id mit
+      einem gültigen Captcha-Token konfiguriert werden (PRIMARY).<br />
+      Das SoC-Modul des Fahrzeuges mit <b>gültigem</b> Captcha-Token führt die SoC-Abfragen für alle Fahrzeuge im
+      CD-Account aus.<br />
+      Die SECONDARY SoC-Module holen sich ihre Daten vom PRIMARY.<br />
+      Bei den weiteren Fahrzeugen ist daher als Captcha-Token der Wert "SECONDARY" (ohne Quotes) einzutragen.<br />
+      Um das Captcha-Token zu bekommen bitte folgende Schritte durchführen:
+      <ol>
+        <li>
+          In einem neuen Browser-Tab auf diese Seite gehen:<br />
+          <a
+            href="https://bimmer-connected.readthedocs.io/en/latest/captcha/rest_of_world.html"
+            target="_blank"
+          >
+            Captcha Lösen
+            <font-awesome-icon :icon="['fas', 'external-link-alt']" />
+          </a>
+          (https://bimmer-connected.readthedocs.io/en/latest/captcha/rest_of_world.html)
+        </li>
+        <li>
+          Dort falls angefragt das Captcha lösen und/oder "Ich bin ein Mensch" und dann Submit anklicken. Als Ergebnis
+          wird ein sehr langer String angezeigt.
+        </li>
+        <li>Diesen String komplett mit <b>Copy&Paste</b> in das Feld Captcha-Token eingeben.</li>
+        <li>Die Konfiguration speichern.</li>
+        <li><b>Sofort einmal im Desktop manuell (Kreispfeil) den SoC für das Fahrzeug abrufen.</b></li>
+        <li><b>Fertig.</b> Ab jetzt wird bis zum nächsten Neustart das sog. Refresh-Token verwendet.</li>
+      </ol>
+      <b>Achtung: Das Captcha-Token kann nur einmal verwendet werden und gilt nur kurze Zeit!</b>
     </openwb-base-alert>
     <openwb-base-text-input
       title="Benutzername"
@@ -41,39 +65,6 @@
       :model-value="vehicle.configuration.captcha_token"
       @update:model-value="updateConfiguration($event, 'configuration.captcha_token')"
     >
-      <template #help>
-        Zum erstmaligen Login z.B. nach einem Neustart oder wenn die Abfrage nicht mehr funktioniert ist ein
-        <b>aktuelles Captcha-Token</b> notwendig.<br />
-        Falls mehr als ein Fahrzeug in dem CD-Account vorhanden ist, sollte nur das mit der niedrigsten openWB id mit
-        einem gültigen Captcha-Token konfiguriert werden (PRIMARY).<br />
-        Das SoC-Modul des Fahrzeuges mit <b>gültigem</b> Captcha-Token führt die SoC-Abfragen für alle Fahrzeuge im
-        CD-Account aus.<br />
-        Die SECONDARY SoC-Module holen sich ihre Daten vom PRIMARY.<br />
-        Bei den weiteren Fahrzeugen ist daher als Captcha-Token der Wert "SECONDARY" (ohne Quotes) einzutragen.<br />
-        Um das Captcha-Token zu bekommen bitte folgende Schritte durchführen:
-        <ol>
-          <li>
-            In einem neuen Browser-Tab auf diese Seite gehen:<br />
-            <a
-              href="https://bimmer-connected.readthedocs.io/en/latest/captcha/rest_of_world.html"
-              target="_blank"
-            >
-              Captcha Lösen
-              <font-awesome-icon :icon="['fas', 'external-link-alt']" />
-            </a>
-            (https://bimmer-connected.readthedocs.io/en/latest/captcha/rest_of_world.html)
-          </li>
-          <li>
-            Dort falls angefragt das Captcha lösen und/oder "Ich bin ein Mensch" und dann Submit anklicken. Als Ergebnis
-            wird ein sehr langer String angezeigt.
-          </li>
-          <li>Diesen String komplett mit <b>Copy&Paste</b> in das Feld Captcha-Token eingeben.</li>
-          <li>Die Konfiguration speichern.</li>
-          <li><b>Sofort einmal im Desktop manuell (Kreispfeil) den SoC für das Fahrzeug abrufen.</b></li>
-          <li><b>Fertig.</b> Ab jetzt wird bis zum nächsten Neustart das sog. Refresh-Token verwendet.</li>
-        </ol>
-        <b>Achtung: Das Captcha-Token kann nur einmal verwendet werden und gilt nur kurze Zeit!</b>
-      </template>
     </openwb-base-text-input>
     <openwb-base-button-group-input
       title="SoC während der Ladung berechnen"
