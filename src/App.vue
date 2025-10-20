de64ebb824dd626c7512f7ec6b608c6bee79005f
BrettS <brett.scott@openwb.de>
2025-10-20 09:27:39 +0200

    Responsive table width for larger screen sizes

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
   </header>
   <div
     role="main"
-    class="container"
+    :class="['container', $route.name === 'ChargeLog' ? 'charge-log-container' : '']"
   >
     <div id="content">
-      <h1>{{ $route.meta.heading }}</h1>
+      <h1 v-if="$route.name !== 'ChargeLog'">{{ $route.meta.heading }}</h1>
       <router-view
         @save="saveValues"
         @reset="resetValues"
@@ -322,5 +322,26 @@ export default {
 
 .bg-pink {
   background-color: var(--pink) !important;
+}
+
+@media (min-width: 1600px) {
+  .charge-log-container {
+    max-width: 1600px !important;
+  }
+}
+@media (min-width: 2000px) {
+  .charge-log-container {
+    max-width: 2000px !important;
+  }
+}
+@media (min-width: 2500px) {
+  .charge-log-container {
+    max-width: 2500px !important;
+  }
+}
+@media (min-width: 3000px) {
+  .charge-log-container {
+    max-width: 3000px !important;
+  }
 }
 </style>
