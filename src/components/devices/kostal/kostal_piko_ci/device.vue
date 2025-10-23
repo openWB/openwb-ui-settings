e19109643599b4042a7e8c5e51b8612326e76bdd
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2025-10-23 14:15:05 +0200

    add kostal piko ci (#769)
    * add kostal piko ci
    
    * improve hint

---
+++ src/components/devices/kostal/kostal_piko_ci/device.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="device-kostal-piko-ci">
+    <openwb-base-heading>Einstellungen für Kostal Piko CI</openwb-base-heading>
+    <openwb-base-alert subtype="info">
+      Die Kommunikation erfolgt via Modbus/TCP.
+    </openwb-base-alert>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-number-input
+      title="Port"
+      required
+      :min="1"
+      :max="65535"
+      :model-value="device.configuration.port"
+      @update:model-value="updateConfiguration($event, 'configuration.port')"
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceKostalPikoCi",
+  mixins: [DeviceConfigMixin],
+};
+</script>
