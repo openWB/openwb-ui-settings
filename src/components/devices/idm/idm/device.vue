8b6b189875e34a9fc4fdc1817ed95a801f17e3c0
SeaSpotter <florian.horch@gmail.com>
2025-10-27 11:59:49 +0100

    Add IDM heatpump as counter (#797)
    * Create device.vue
    
    * Create counter.vue
    
    * Delete counter.vue

---
+++ src/components/devices/idm/idm/device.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="device-idm">
+    <openwb-base-heading> Einstellungen für IDM </openwb-base-heading>
+    <openwb-base-alert subtype="info">
+      ModbusTCP muss in der Fachmannebene aktiviert sein.
+    </openwb-base-alert>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-number-input
+      title="Port"
+      required
+      :min="1"
+      :max="65535"
+      :model-value="device.configuration.port"
+      @update:model-value="updateConfiguration($event, 'configuration.port')"
+    />
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="device.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceIDM",
+  mixins: [DeviceConfigMixin],
+};
+</script>
