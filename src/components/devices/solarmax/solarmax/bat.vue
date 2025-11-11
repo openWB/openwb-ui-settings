358b176be28e3e7a84f3f526a537361b028e1ff0
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2025-11-11 16:00:11 +0100

    add components (#838)

--- src/components/devices/solarmax/solarmax/bat.vue
+++ src/components/devices/solarmax/solarmax/bat.vue
@@ -2,7 +2,10 @@
   <div class="device-solarmax-bat">
     <openwb-base-alert subtype="info">
       Solarmax MAX.STORAGE / MAX.STORAGE Ultimate Batteriespeicher sind Teil eines Hybridsystems. Daher muss auch eine
-      Komponente Wechselrichter angelegt werden.
+      Komponente Wechselrichter angelegt werden.<br />
+      Die aktive Speichersteuerung ist ab Solarmax Software Version 3.4.4 verfügbar.
+      Zur Nutzung muss die Funktion "Connectivity+" durch den Solarmax Support (nicht openWB Support)
+      freigeschaltet werden.
     </openwb-base-alert>
     <openwb-base-number-input
       title="Modbus ID"
@@ -11,6 +14,23 @@
       min="1"
       max="255"
       @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+    <openwb-base-button-group-input
+      title="Speicherleistung steuerbar"
+      :buttons="[
+        {
+          buttonValue: false,
+          text: 'Nein',
+          class: 'btn-outline-danger',
+        },
+        {
+          buttonValue: true,
+          text: 'Ja',
+          class: 'btn-outline-success',
+        },
+      ]"
+      :model-value="component.configuration.power_limit_controllable"
+      @update:model-value="updateConfiguration($event, 'configuration.power_limit_controllable')"
     />
   </div>
 </template>
---
+++ src/components/devices/solarmax/solarmax/inverter_maxstorage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="device-solarmax-inverter-maxstorage">
+    <openwb-base-heading> Einstellungen für Solarmax MAX.STORAGE / MAX.STORAGE Ultimate Wechselrichter </openwb-base-heading>
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="component.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceSolarmaxMsInverter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
