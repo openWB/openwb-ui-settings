2ae07957c701946f5a47379c48bed25acf94758d
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2025-11-18 08:24:07 +0100

    zcs 3p and chint settings (#848)

---
+++ src/components/devices/chint/chint/counter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="device-chint-counter">
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="device.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceChintCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
