96f549fce5780cb0cd2fd65d4ba3e7a54f53c89d
Lutz Bender <github@lutz-bender.de>
2025-11-25 09:46:05 +0100

    implement disabled state for button group component

--- src/components/OpenwbBaseButtonGroupInput.vue
+++ src/components/OpenwbBaseButtonGroupInput.vue
@@ -17,7 +17,12 @@
           v-for="button in buttons"
           :key="button.value"
           class="btn btn-same-size btn-centered"
-          :class="[value == button.buttonValue ? 'active' : '', button.class ? button.class : 'btn-outline-info']"
+          :disabled="disabled"
+          :class="[
+            { active: value == button.buttonValue },
+            { disabled: disabled },
+            button.class ? button.class : 'btn-outline-info',
+          ]"
         >
           <span>
             <input
@@ -25,6 +30,7 @@
               type="radio"
               :value="button.buttonValue"
               v-bind="$attrs"
+              :disabled="disabled"
               @click="$emit('button-click', button.buttonValue)"
             />
             <slot :name="'label-' + button.buttonValue">
@@ -62,6 +68,7 @@ export default {
     title: { type: String, required: false, default: "" },
     modelValue: { type: [String, Number, Boolean], default: undefined },
     buttons: { type: Array, required: true },
+    disabled: { type: Boolean, required: false, default: false },
   },
   emits: ["update:modelValue", "button-click"],
   computed: {
