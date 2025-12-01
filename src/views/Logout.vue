9ed5f8e2bfab6a2f388503766876cf09b3e9aa2f
Lutz Bender <github@lutz-bender.de>
2025-12-01 10:41:37 +0100

    user pages

---
+++ src/views/Logout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="logout">
+    <openwb-base-alert subtype="info">
+      Du wurdest abgemeldet.
+      <router-link to="/Login"> Neu Anmelden. </router-link>
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OpenwbLogoutView",
+  data() {
+    return {
+      mqttTopicsToSubscribe: [],
+    };
+  },
+  mounted() {
+    // unset cookie
+    console.warn("Logging out...");
+    this.$cookies.remove("mqtt");
+    // reconnect mqtt
+    this.$root.reconnectMqttClient();
+    this.$store.commit("storeLocal", { name: "username", value: null });
+  },
+};
+</script>
