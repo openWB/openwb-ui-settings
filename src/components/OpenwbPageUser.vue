5f9ffb3b9afe04b0545c007a9d840914c746f2b9
Lutz Bender <github@lutz-bender.de>
2025-12-01 12:00:14 +0100

    refactor info bar

---
+++ src/components/OpenwbPageUser.vue
@@ -0,0 +1,67 @@
+<template>
+  <teleport
+    defer
+    to="#infoBar"
+  >
+    <div
+      id="user-indicator"
+      class="text-light clickable"
+    >
+      <router-link
+        v-if="loggedInUser"
+        to="/Logout"
+        class="text-light px-2"
+        active-class="active disabled"
+      >
+        <FontAwesomeIcon
+          :icon="['fas', 'circle-user']"
+          :title="loggedInUser"
+        />
+      </router-link>
+      <router-link
+        v-else
+        to="/Login"
+        class="text-light px-2"
+        active-class="active disabled"
+      >
+        <FontAwesomeIcon
+          :icon="['fas', 'arrow-right-to-bracket']"
+          title="Anmelden"
+        />
+      </router-link>
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faCircleUser as fasCircleUser,
+  faArrowRightToBracket as fasArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(fasCircleUser, fasArrowRightToBracket);
+
+export default {
+  name: "OpenwbPageUser",
+  components: {
+    FontAwesomeIcon,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    loggedInUser() {
+      return this.$store.state.local.username || null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#user-indicator {
+  font-weight: bolder;
+  font-size: 175%;
+}
+</style>
