9df4c3918f7f1b7f75629242a23f64d244d99303
LKuemmel <lena.kuemmel@openwb.de>
2025-12-05 09:30:10 +0100

    Solakon

---
+++ src/components/devices/solakon/solakon_one/device.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="device-solakon-one">
+    <openwb-base-heading>Einstellungen für Solakon One</openwb-base-heading>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-number-input
+      title="Port"
+      required
+      :min="1"
+      :max="65535"
+      :model-value="device.configuration.port"
+      @update:model-value="updateConfiguration($event, 'configuration.port')"
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceSolakonOne",
+  mixins: [DeviceConfigMixin],
+};
+</script>
