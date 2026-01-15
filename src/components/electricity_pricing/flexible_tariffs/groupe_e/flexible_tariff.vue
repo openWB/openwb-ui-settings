01fd458823de0c948d69100c55e307f668999c78
Claus Hagen <cshagen@hagens.ch>
2026-01-15 13:13:56 +0100

    fix config pages for swiss tariffs

---
+++ src/components/electricity_pricing/flexible_tariffs/groupe_e/flexible_tariff.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flexible-tariff-groupe-e">
+    <openwb-base-alert subtype="info">
+      Tarif Vario der Groupe E (Westschweiz) - www.groupe-e.ch/de/strom/vario
+      <br />
+      Der Tarif passt sich alle 15 Minuten an, abhängig von der aktuellen Netzauslastung und dem Marktpreis.
+      <br />
+      Die Preise für den nächsten Tag werden jeweils am Vortag um 18 Uhr publiziert.
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import FlexibleTariffConfigMixin from "../FlexibleTariffConfigMixin.vue";
+
+export default {
+  name: "FlexibleTariffGroupeE",
+  mixins: [FlexibleTariffConfigMixin],
+};
+</script>
