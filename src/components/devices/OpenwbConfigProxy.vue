556a1da823df80b241faf539263807ebab28f6cf
BrettS <brett.scott@openwb.de>
2026-01-30 09:54:37 +0100

    Add optional chaining

--- src/components/devices/OpenwbConfigProxy.vue
+++ src/components/devices/OpenwbConfigProxy.vue
@@ -26,7 +26,7 @@ export default {
       console.debug(`loading component: ${this.device?.type} / ${this.component?.type}`);
       if (this.component !== undefined) {
         return defineAsyncComponent({
-          loader: () => import(`./${this.device.vendor}/${this.device.type}/${this.component.type}.vue`),
+          loader: () => import(`./${this.device?.vendor}/${this.device?.type}/${this.component?.type}.vue`),
           errorComponent: OpenwbComponentConfigFallback,
         });
       } else {
