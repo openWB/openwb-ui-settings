24cf061531af2d2e724c1bdbfd4964eedbe201a4
LKuemmel <lena.kuemmel@openwb.de>
2026-02-10 15:01:29 +0100

    user, password and url for dc adapter

--- src/components/charge_points/openwb_dc_adapter/chargePoint.vue
+++ src/components/charge_points/openwb_dc_adapter/chargePoint.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="charge-point-openwb-dc-adapter">
     <openwb-base-text-input
-      title="IP oder Hostname"
-      subtype="host"
+      title="URL"
+      subtype="url"
       required
-      :model-value="chargePoint.configuration.ip_address"
-      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+      :model-value="chargePoint.configuration.url"
+      @update:model-value="updateConfiguration($event, 'configuration.url')"
+    />
+    <openwb-base-text-input
+      title="Benutzername"
+      subtype="user"
+      :model-value="chargePoint.configuration.user"
+      @update:model-value="updateConfiguration($event, 'configuration.user')"
+    />
+    <openwb-base-text-input
+      title="Passwort"
+      subtype="password"
+      :model-value="chargePoint.configuration.password"
+      @update:model-value="updateConfiguration($event, 'configuration.password')"
     />
   </div>
 </template>
