32cc0378e7939f924a8af949a1029ebdeddc066a
AlexHuebi <alexander@huebinger.at>
2026-02-13 17:22:07 +0100

    add config page

---
+++ src/components/devices/victron/victron_3p75ct/device.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="device-victron">
+    <openwb-base-heading> Einstellungen für Victron 3P75CT </openwb-base-heading>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-number-input
+      title="Port"
+      required
+      :min="1"
+      :max="65535"
+      :model-value="device.configuration.port"
+      @update:model-value="updateConfiguration($event, 'configuration.port')"
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceVictron3P75CT",
+  mixins: [DeviceConfigMixin],
+};
+</script>
