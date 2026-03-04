755b80b383774d3272ec82311844dcf096169727
BrettS <brett.scott@openwb.de>
2026-03-04 13:06:11 +0100

    add optional chaining

--- src/components/mixins/ComponentState.vue
+++ src/components/mixins/ComponentState.vue
@@ -19,7 +19,7 @@ export default {
     },
     systemVersion() {
       const version = this.$store.state.mqtt["openWB/system/version"];
-      const match = version.match(/^\d+\.\d+\.\d+/);
+      const match = version?.match(/^\d+\.\d+\.\d+/);
       return match ? match[0] : "2.1.8";
     },
   },
