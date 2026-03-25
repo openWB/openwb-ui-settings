76df7c03646543955390c4a0762408ea6f53dddd
LKuemmel <lena.kuemmel@openwb.de>
2026-03-25 11:45:47 +0100

    Improve help manual soc

--- src/components/vehicles/VehicleConfigMixin.vue
+++ src/components/vehicles/VehicleConfigMixin.vue
@@ -1,5 +1,12 @@
 <script>
 export default {
+  computed: {
+    systemVersion() {
+      const version = this.$store.state.mqtt["openWB/system/version"];
+      const match = version?.match(/^\d+\.\d+\.\d+/);
+      return match ? match[0] : "2.1.8";
+    },
+  },
   props: {
     vehicleId: { required: true, type: Number },
     vehicle: { required: true, type: Object },
--- src/components/vehicles/manual/vehicle.vue
+++ src/components/vehicles/manual/vehicle.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="vehicle-soc-manual">
     <openwb-base-alert subtype="info">
-      Beim Anstecken des Fahrzeugs muss der aktuelle SoC über die Hauptseite angegeben werden. Anhand des Zählers im
-      Ladepunkt wird dann der aktuelle SoC errechnet. Ausschlaggebend für die Qualität dieses Moduls sind die beiden
-      Einstellungen "Kapazität der Batterie" und "Wirkungsgrad der Ladeelektronik" in dem Fahrzeug-Profil.
+      Beim Anstecken des Fahrzeugs muss der aktuelle SoC-Wert des Fahrzeugs über die Hauptseite oder (wenn verbaut) am
+      Display der openWB angegeben werden. Anhand des geladenen Stroms wird der aktuelle SoC-Wert des Fahrzeugs während
+      der Ladung berechnet. Ausschlaggebend für die Genauigkeit der Berechnung sind die beiden Einstellungen "Kapazität
+      der Batterie" und "Wirkungsgrad der Ladeelektronik" im Fahrzeug-Profil. Weitere Infos dazu im
+      <a
+        :href="`https://wiki.openwb.de/doku.php?id=openwb:vc:${systemVersion}:software:fahrzeug-infos:manueller-soc`"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Wiki-Beitrag zum manuellen SoC </a
+      >.<br />
     </openwb-base-alert>
   </div>
 </template>
