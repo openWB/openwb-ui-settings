0d1335b8c09cdeb6dc01cd71957fb022cd76410d
LKuemmel <76958050+LKuemmel@users.noreply.github.com>
2026-03-25 12:08:57 +0100

    optional reset of the manual SoC (#924)
    * draft
    
    * improve
    
    * typo

--- src/components/vehicles/manual/vehicle.vue
+++ src/components/vehicles/manual/vehicle.vue
@@ -5,6 +5,22 @@
       Ladepunkt wird dann der aktuelle SoC errechnet. Ausschlaggebend für die Qualität dieses Moduls sind die beiden
       Einstellungen "Kapazität der Batterie" und "Wirkungsgrad der Ladeelektronik" in dem Fahrzeug-Profil.
     </openwb-base-alert>
+    <openwb-base-button-group-input
+      title="SoC ohne manuelle Eingabe"
+      :buttons="[
+        {
+          buttonValue: false,
+          text: 'letzten SoC beibehalten',
+        },
+        {
+          buttonValue: true,
+          text: 'nach dem Abstecken auf 0% zurücksetzen',
+        },
+      ]"
+      :model-value="vehicle.configuration.reset_after_unplug"
+      @update:model-value="updateConfiguration($event, 'configuration.reset_after_unplug')"
+    >
+    </openwb-base-button-group-input>
   </div>
 </template>
 
