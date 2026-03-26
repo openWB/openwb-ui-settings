66957bab395cedb1e333f7f2b7ddcd16ceb9241f
Xoffroad <offroad@posteo.de>
2026-03-26 15:30:18 +0100

    remove field for max_power, show info text only

--- src/components/devices/qcells/qcells/bat.vue
+++ src/components/devices/qcells/qcells/bat.vue
@@ -1,32 +1,17 @@
 <template>
   <div class="device-qcells-bat">
     <openwb-base-alert subtype="info">
-      Die aktive Speichersteuerung erfolgt über das Solax Remote Control Modbus-Protokoll (Mode 8).<br />
-      Unterstützte Hardware: QCells Q.VOLT HYB-G3-3P (Solax Gen4), Solax Gen4/Gen5/Gen6 Hybrid und AC
-      Wechselrichter.<br />
-      Gen2/Gen3 Wechselrichter werden nicht unterstützt, da diese kein Remote Control unterstützen.
+      Die aktive Speichersteuerung erfolgt über das Solax Remote Control
+      Modbus-Protokoll (Mode 8).<br />
+      Unterstützte Hardware: QCells Q.VOLT HYB-G3-3P (Solax Gen4),
+      Solax Gen4/Gen5/Gen6 Hybrid und AC Wechselrichter.<br />
+      Gen2/Gen3 Wechselrichter werden nicht unterstützt, da diese kein
+      Remote Control unterstützen.<br />
+      Bitte die Felder "Maximale Ladeleistung" und "Maximale
+      Entladeleistung" unter Ladeeinstellungen &gt; Speichersteuerung
+      entsprechend der Leistungsfähigkeit des Wechselrichters und der
+      angeschlossenen Batterie konfigurieren.
     </openwb-base-alert>
-    <openwb-base-alert subtype="warning">
-      Die maximale Lade-/Entladeleistung muss der tatsächlichen Leistungsfähigkeit des
-      Wechselrichters und der angeschlossenen Batterie entsprechen. Nicht kompatible Werte
-      können zu Schäden an der Batterie oder dem Wechselrichter führen.
-    </openwb-base-alert>
-    <openwb-base-number-input
-      title="Maximale Lade-/Entladeleistung"
-      required
-      :min="0"
-      :max="10000"
-      :step="100"
-      unit="W"
-      :model-value="component.configuration.max_power"
-      @update:model-value="updateConfiguration($event, 'configuration.max_power')"
-    >
-      <template #help>
-        Maximale Lade- und Entladeleistung des Speichers in Watt. Der Wert sollte der maximalen
-        Lade-/Entladeleistung des Wechselrichters entsprechend der angeschlossenen Batteriekapazität
-        entsprechen.
-      </template>
-    </openwb-base-number-input>
   </div>
 </template>
 
