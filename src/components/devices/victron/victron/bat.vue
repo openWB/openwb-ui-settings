9968b2bad649001cbf7639440d8e9e0df2d6943f
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2026-04-01 12:58:27 +0200

    remove vebus_id (#940)

--- src/components/devices/victron/victron/bat.vue
+++ src/components/devices/victron/victron/bat.vue
@@ -9,19 +9,6 @@
       max="255"
       @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
     />
-    <openwb-base-number-input
-      title="Modbus ID VE.Bus"
-      required
-      :model-value="component.configuration.vebus_id"
-      min="1"
-      max="255"
-      @update:model-value="updateConfiguration($event, 'configuration.vebus_id')"
-    >
-      <template #help>
-        Die Modbus ID des VE.Bus kann je nach Victron Version abweichen. Übliche IDs sind 227, 228, 242 und 246.<br />
-        Die ID kann unter "Settings > Services > Modbus/TCP > Available services" eingesehen werden.
-      </template>
-    </openwb-base-number-input>
   </div>
 </template>
 
