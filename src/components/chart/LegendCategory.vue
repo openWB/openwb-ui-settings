723222c4a7ba448c7bac20eaaa3d86bda03cc7af
BrettS <brett.scott@openwb.de>
2026-04-13 11:25:47 +0200

    Set maximum height for legend category

--- src/components/chart/LegendCategory.vue
+++ src/components/chart/LegendCategory.vue
@@ -8,7 +8,7 @@
     >
       {{ label }}
     </button>
-    <ul class="dropdown-menu">
+    <ul class="dropdown-menu legend-dropdown-scroll">
       <li
         v-for="item in items"
         :key="item.label"
@@ -62,5 +62,9 @@ export default {
 }
 .legend-item-hidden {
   opacity: 0.5;
+}
+.legend-dropdown-scroll {
+  max-height: 500px;
+  overflow-y: auto;
 }
 </style>
