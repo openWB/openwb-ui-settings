ca9085b332952ae24b7ff92c53a7060e849c0153
SeaSpotter <florian.horch@gmail.com>
2026-04-14 10:05:51 +0200

    Add counter component for Anker device configuration

---
+++ src/components/devices/anker/anker_solix/counter.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="device-anker_solix-counter">
+    <openwb-base-heading> Einstellungen für Anker Zähler </openwb-base-heading>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="component.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    />
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceAnkerCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
