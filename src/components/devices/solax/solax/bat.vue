4931416c42af9db6f5d93ef3709ac5bbc65e7c37
Xoffroad <offroad@posteo.de>
2026-05-07 10:46:56 +0200

    Add battery control info component for SolaX.
    Add a dedicated SolaX battery settings/info component describing Mode4 active control support.

---
+++ src/components/devices/solax/solax/bat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="device-solax-bat">
+    <openwb-base-alert subtype="info">
+      Die aktive Speichersteuerung wird nur für Solax "Gen 3 &amp; Gen 4 Hybrid"
+      (z.B. X3-Hybrid-G4) unterstützt.<br />
+      Die Steuerung erfolgt über das Solax Remote Control Modbus-Protokoll
+      (Mode 4, Push Power-Positive/Negative).<br />
+      Bitte die Felder "Maximale Ladeleistung" und "Maximale
+      Entladeleistung" unter Ladeeinstellungen &gt; Speichersteuerung
+      entsprechend der Leistungsfähigkeit des Wechselrichters und der
+      angeschlossenen Batterie konfigurieren.
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceSolaxBat",
+  mixins: [ComponentConfigMixin],
+};
+</script>
