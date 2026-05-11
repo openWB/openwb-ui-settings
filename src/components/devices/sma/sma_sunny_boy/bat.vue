bfc708674450a4fa191edf458a638c40624823c8
SeaSpotter <florian.horch@gmail.com>
2026-05-11 12:44:02 +0200

    Add version selection input for SMA Sunny Boy

--- src/components/devices/sma/sma_sunny_boy/bat.vue
+++ src/components/devices/sma/sma_sunny_boy/bat.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="device-sunnyboy-bat">
     <openwb-base-heading> Einstellungen für SMA Sunny Boy / Tripower / Tesvolt Batteriespeicher </openwb-base-heading>
+    <openwb-base-select-input
+      title="Version"
+      not-selected="Bitte auswählen"
+      :options="[
+        { value: 0, text: 'Hybrid Wechselrichter' },
+        { value: 1, text: 'Sunny Boy Storage' },
+        { value: 2, text: 'Tesvolt' },
+      ]"
+      :model-value="component.configuration.version"
+      required
+      @update:model-value="updateConfiguration($event, 'configuration.version')"
+    />
     <openwb-base-number-input
       title="Modbus ID"
       required
