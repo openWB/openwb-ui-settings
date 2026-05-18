4051641273945bcdb490591423c1e6dcefc769c2
BrettS <brett.scott@openwb.de>
2026-05-18 14:18:25 +0200

    Remove price from shop info card

--- src/components/billing/tenantBillingShop.vue
+++ src/components/billing/tenantBillingShop.vue
@@ -6,29 +6,21 @@
       <strong>egal ob mit oder ohne PV-Anlage.</strong> Auch gespeicherter Strom (z.B. aus einem Batteriespeicher) kann
       berücksichtigt werden.
     </div>
-    <div class="mb-3">
-      <div class="font-weight-bold">Vorteile:</div>
-      <ul class="mb-2 pl-3">
-        <li>Günstiger Strom durch Wegfall von Netzentgelten</li>
-        <li>Abrechnung pro Wohneinheit</li>
-        <li>Klare Zuordnung von PV-/Netz-/Speicherstrom</li>
-        <li>RFID Ladefreigabe</li>
-        <li>Monatliche oder jährliche Abrechnung</li>
-      </ul>
-    </div>
-    <div class="mb-3">
-      <div class="font-weight-bold">Kosten:</div>
-      <div class="row align-items-center">
-        <div class="col"><strong>5,00 € im Monat</strong> je Abrechnungsstelle</div>
-        <div class="col-auto">
-          <openwb-base-click-button
-            class="btn btn-outline-secondary"
-            href="https://wb-solution.de/produkt/mieterstromabrechnung/"
-          >
-            JETZT BESTELLEN >
-          </openwb-base-click-button>
-        </div>
-      </div>
+    <div class="font-weight-bold">Vorteile:</div>
+    <ul class="mb-2 pl-3">
+      <li>Günstiger Strom durch Wegfall von Netzentgelten</li>
+      <li>Abrechnung pro Wohneinheit</li>
+      <li>Klare Zuordnung von PV-/Netz-/Speicherstrom</li>
+      <li>RFID Ladefreigabe</li>
+      <li>Monatliche oder jährliche Abrechnung</li>
+    </ul>
+    <div class="d-flex justify-content-end">
+      <openwb-base-click-button
+        class="btn btn-outline-secondary order-button"
+        href="https://wb-solution.de/produkt/mieterstromabrechnung/"
+      >
+        JETZT BESTELLEN >
+      </openwb-base-click-button>
     </div>
   </openwb-base-card>
 </template>
@@ -37,3 +29,8 @@ export default {
   name: "TenantBillingShop",
 };
 </script>
+<style scoped>
+.order-button {
+  width: auto;
+}
+</style>
