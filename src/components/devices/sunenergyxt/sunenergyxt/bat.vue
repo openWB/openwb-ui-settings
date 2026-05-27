7613794e2a704e9c055d51e7ac8354e7e7251f02
Christoph <Christoph@Caina.de>
2026-05-28 01:20:32 +0200

    Add bat.vue component for SunEnergyXT

---
+++ src/components/devices/sunenergyxt/sunenergyxt/bat.vue
@@ -0,0 +1,10 @@
+<template>
+  <div />
+</template>
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+export default {
+  name: "SunEnergyXTBat",
+  mixins: [ComponentConfigMixin],
+};
+</script>
