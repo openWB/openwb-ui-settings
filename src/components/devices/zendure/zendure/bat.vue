bd38f16329ea4584955508e94156b04fceeadcfc
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2026-06-10 14:13:42 +0200

    add zendure (#993)

---
+++ src/components/devices/zendure/zendure/bat.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Einstellungen sind vorbelegt und sollen nicht im UI angezeigt werden -->
+  <div class="device-zendure-bat"></div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceZendureBat",
+  mixins: [ComponentConfigMixin],
+};
+</script>
---
+++ src/components/devices/zendure/zendure/device.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="device-zendure">
+    <openwb-base-heading>Einstellungen für Zendure</openwb-base-heading>
+    <openwb-base-text-input
+      title="IP oder Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.url"
+      @update:model-value="updateConfiguration($event, 'configuration.url')"
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceZendure",
+  mixins: [DeviceConfigMixin],
+};
+</script>
