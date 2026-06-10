c33dffff97be49f94717f6f69d54303f818b1a70
ndrsnhs <andreas.neuhaus@openwb.de>
2026-06-10 13:49:20 +0200

    add zendure

---
+++ src/components/devices/zendure/zendure/counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Einstellungen sind vorbelegt und sollen nicht im UI angezeigt werden -->
+  <div class="device-zendure-counter"></div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceZendureCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
---
+++ src/components/devices/zendure/zendure/inverter.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- Einstellungen sind vorbelegt und sollen nicht im UI angezeigt werden -->
+  <div class="device-zendure-inverter"></div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceZendureInverter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
