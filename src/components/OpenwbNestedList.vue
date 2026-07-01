27fea1b65ef40634f65cd1e79484ded4cd7b31e4
BrettS <168732306+Brett-S-OWB@users.noreply.github.com>
2026-07-01 09:11:50 +0200

    Loadmanagement prio list (#969)
    * Implement flat sortable table - loadmanagement priority
    
    * Remove lightning symbol full power concept
    
    * Add table nesting depth restriction - (maximum)
    
    * Prevent dragging one group inside another
    
    * Add user defined group - fix labels
    
    * Add group delete button - prevent group nesting
    
    * Make group name editable - change group color
    
    * Formatting
    
    * Add editing icon
    
    * Add help text
    
    * fix undefined bug on pressing discard changes
    
    * remove import
    
    * Remove backward compatibility prio-list
    
    * rename group functions
    
    * remove comment
    
    * rename functions
    
    * fix consumer labels and colors
    
    * add icons to consumers and vehicles

--- src/components/OpenwbNestedList.vue
+++ src/components/OpenwbNestedList.vue
@@ -1,9 +1,9 @@
 <template>
   <draggable
+    v-model="list"
     class="dragArea w-100 mb-0"
     tag="ul"
-    :list="list"
-    :group="{ name: 'g1' }"
+    :group="dragGroup"
     item-key="id"
     handle=".handle"
   >
@@ -22,21 +22,45 @@
               v-if="getElementIcon(element)"
               :icon="getElementIcon(element)"
             />
-            {{ getElementLabel(element.id) }}
-          </span>
-          <!-- <span class="element-actions">
-            <font-awesome-icon
-
-              :icon="['fas', 'edit']"
-              @click="elementEdit(element.id)"
+            <span v-if="editingGroupId !== element.id">
+              <span
+                style="cursor: pointer"
+                @click="startEditing(element)"
+              >
+                {{ getElementLabel(element.id) }}
+              </span>
+            </span>
+            <input
+              v-else
+              v-model="editingValue"
+              class="group-rename-input"
+              @keyup.enter="finishEditing(element.id)"
+              @blur="finishEditing(element.id)"
             />
-          </span> -->
+          </span>
+          <span
+            v-if="element.type === 'group'"
+            class="element-actions"
+          >
+            <font-awesome-icon
+              class="mr-2"
+              :icon="['fas', 'pen']"
+              @click.stop="startEditing(element)"
+            />
+            <font-awesome-icon
+              :icon="['fas', 'trash']"
+              @click.stop="$emit('delete-group', element.id)"
+            />
+          </span>
         </div>
         <openwb-nested-list
-          v-if="nesting && element.children"
+          v-if="nesting && element.children && currentNestingDepth < maxNestingDepth"
           v-model="element.children"
           :labels="labels"
           :nesting="nesting"
+          :max-nesting-depth="maxNestingDepth"
+          :current-nesting-depth="currentNestingDepth + 1"
+          @delete-group="$emit('delete-group', $event)"
         />
       </li>
     </template>
@@ -54,11 +78,25 @@ import {
   faCarBattery as fasCarBattery,
   faSolarPanel as fasSolarPanel,
   faGaugeHigh as fasGaugeHigh,
+  faTrash as fasTrash,
+  faPen as fasPen,
+  faCar as fasCar,
+  faPlug as fasPlug,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(fasArrowsAlt, fasArrowsUpDown, fasChargingStation, fasCarBattery, fasSolarPanel, fasGaugeHigh);
-
+library.add(
+  fasArrowsAlt,
+  fasArrowsUpDown,
+  fasChargingStation,
+  fasCarBattery,
+  fasSolarPanel,
+  fasGaugeHigh,
+  fasTrash,
+  fasPen,
+  fasCar,
+  fasPlug,
+);
 export default {
   name: "OpenwbNestedList",
   components: {
@@ -66,9 +104,46 @@ export default {
     FontAwesomeIcon,
   },
   props: {
-    list: { type: Object, required: false, default: undefined },
+    modelValue: { type: Array, required: false, default: () => [] },
     labels: { type: Object, required: false, default: undefined },
     nesting: { type: Boolean, default: true },
+    maxNestingDepth: { type: Number, default: Infinity },
+    currentNestingDepth: { type: Number, default: 0 },
+  },
+  emits: ["update:modelValue", "delete-group", "rename-group"],
+  data() {
+    return {
+      editingGroupId: null,
+      editingValue: "",
+    };
+  },
+  computed: {
+    list: {
+      get() {
+        return this.modelValue;
+      },
+      set(val) {
+        this.$emit("update:modelValue", val);
+      },
+    },
+    dragGroup() {
+      if (this.currentNestingDepth === 0) {
+        return {
+          name: "g1",
+          pull: true,
+          put: true,
+        };
+      }
+      return {
+        name: "g1",
+        pull: true,
+        put: (to, from, dragEl) => {
+          const draggedItem = dragEl?.__draggable_context?.element;
+          if (!draggedItem) return true;
+          return draggedItem.type !== "group";
+        },
+      };
+    },
   },
   methods: {
     classes(element) {
@@ -100,11 +175,39 @@ export default {
           return ["fas", "gauge-high"];
         case "cp":
           return ["fas", "charging-station"];
+        case "vehicle":
+          return ["fas", "car"];
+        case "consumer":
+          return ["fas", "plug"];
         case "inverter":
           return ["fas", "solar-panel"];
         default:
           return undefined;
       }
+    },
+    startEditing(element) {
+      if (element.type !== "group") return;
+
+      this.editingGroupId = element.id;
+      this.editingValue = element.label;
+
+      this.$nextTick(() => {
+        const input = this.$el.querySelector(".group-rename-input");
+        input?.focus();
+        input?.select();
+      });
+    },
+
+    finishEditing(groupId) {
+      if (!this.editingValue.trim()) {
+        this.editingGroupId = null;
+        return;
+      }
+      this.$emit("rename-group", {
+        id: groupId,
+        label: this.editingValue.trim(),
+      });
+      this.editingGroupId = null;
     },
   },
 };
@@ -168,6 +271,24 @@ export default {
 .element-titel.battery {
   background-color: var(--warning);
   color: var(--dark);
+}
+
+.element-titel.consumer {
+  background-color: var(--purple);
+}
+
+.element-titel.group {
+  background-color: var(--secondary);
+}
+
+.group-rename-input {
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid var(--light);
+  color: var(--light);
+  font: inherit;
+  outline: none;
+  width: 100%;
 }
 
 .element-actions {
