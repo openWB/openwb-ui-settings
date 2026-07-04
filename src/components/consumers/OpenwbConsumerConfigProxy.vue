06e5009dd977f2a1698f1f7e4c2c34ac47ae6d07
Brett-S-OWB <brett.scott@openwb.de>
2026-07-04 20:39:40 +0200

    fix consumer config proxy for project build

--- src/components/consumers/OpenwbConsumerConfigProxy.vue
+++ src/components/consumers/OpenwbConsumerConfigProxy.vue
@@ -27,28 +27,23 @@ export default {
     };
   },
   methods: {
-    getDeviceComponentPath() {
-      if (this.SHARED_DEVICE_VENDORS.includes(this.device.vendor)) {
-        return `../devices/${this.device.vendor}/${this.device.type}/device.vue`;
-      }
-      return `./${this.device.vendor}/${this.device.type}/consumer.vue`;
-    },
-    getComponentPath() {
-      if (this.SHARED_DEVICE_VENDORS.includes(this.device.vendor)) {
-        return `../devices/${this.device.vendor}/${this.device.type}/${this.component.type}.vue`;
-      }
-      return `./${this.device.vendor}/${this.device.type}/${this.component.type}.vue`;
-    },
     getComponent() {
       console.debug(`loading component: ${this.device.type} / ${this.component?.type}`);
+      const isSharedVendor = this.SHARED_DEVICE_VENDORS.includes(this.device.vendor);
       if (this.component !== undefined) {
         return defineAsyncComponent({
-          loader: () => import(/* @vite-ignore */ this.getComponentPath()),
+          loader: () =>
+            isSharedVendor
+              ? import(`../devices/${this.device.vendor}/${this.device.type}/${this.component.type}.vue`)
+              : import(`./${this.device.vendor}/${this.device.type}/${this.component.type}.vue`),
           errorComponent: OpenwbConsumerComponentConfigFallback,
         });
       } else {
         return defineAsyncComponent({
-          loader: () => import(/* @vite-ignore */ this.getDeviceComponentPath()),
+          loader: () =>
+            isSharedVendor
+              ? import(`../devices/${this.device.vendor}/${this.device.type}/device.vue`)
+              : import(`./${this.device.vendor}/${this.device.type}/consumer.vue`),
           errorComponent: OpenwbConsumerDeviceConfigFallback,
         });
       }
