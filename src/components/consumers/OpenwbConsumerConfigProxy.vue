06653ba9ac105c2848dfc485a93d8e85dbd5a7ed
BrettS <168732306+Brett-S-OWB@users.noreply.github.com>
2026-07-06 11:34:23 +0200

    Add info text for consumer switch-on threshold (#1003)
    * formatting
    
    * add info text switch-on threshold
    
    * rename settings card for surplus only
    
    * fix consumer config proxy for project build
    
    * remove redundant component branch of consumer config proxy
    
    * update info text

--- src/components/consumers/OpenwbConsumerConfigProxy.vue
+++ src/components/consumers/OpenwbConsumerConfigProxy.vue
@@ -2,9 +2,6 @@
   <component
     :is="getComponent()"
     :device="device"
-    :component="component"
-    :component-id="component ? component.id : undefined"
-    :component-type="component ? component.type : undefined"
     @update:configuration="updateConfiguration($event)"
   />
 </template>
@@ -12,13 +9,11 @@
 <script>
 import { defineAsyncComponent } from "vue";
 import OpenwbConsumerDeviceConfigFallback from "./OpenwbConsumerDeviceConfigFallback.vue";
-import OpenwbConsumerComponentConfigFallback from "./OpenwbConsumerComponentConfigFallback.vue";
 
 export default {
   name: "OpenwbConsumerConfigProxy",
   props: {
     device: { type: Object, required: true },
-    component: { type: Object, required: false, default: undefined },
   },
   emits: ["update:configuration"],
   data() {
@@ -27,31 +22,16 @@ export default {
     };
   },
   methods: {
-    getDeviceComponentPath() {
-      if (this.SHARED_DEVICE_VENDORS.includes(this.device.vendor)) {
-        return `../devices/${this.device.vendor}/${this.device.type}/device.vue`;
-      }
-      return `./${this.device.vendor}/${this.device.type}/consumer.vue`;
-    },
-    getComponentPath() {
-      if (this.SHARED_DEVICE_VENDORS.includes(this.device.vendor)) {
-        return `../devices/${this.device.vendor}/${this.device.type}/${this.component.type}.vue`;
-      }
-      return `./${this.device.vendor}/${this.device.type}/${this.component.type}.vue`;
-    },
     getComponent() {
-      console.debug(`loading component: ${this.device.type} / ${this.component?.type}`);
-      if (this.component !== undefined) {
-        return defineAsyncComponent({
-          loader: () => import(/* @vite-ignore */ this.getComponentPath()),
-          errorComponent: OpenwbConsumerComponentConfigFallback,
-        });
-      } else {
-        return defineAsyncComponent({
-          loader: () => import(/* @vite-ignore */ this.getDeviceComponentPath()),
-          errorComponent: OpenwbConsumerDeviceConfigFallback,
-        });
-      }
+      console.debug(`loading consumer config: ${this.device.vendor} / ${this.device.type}`);
+      const isSharedVendor = this.SHARED_DEVICE_VENDORS.includes(this.device.vendor);
+      return defineAsyncComponent({
+        loader: () =>
+          isSharedVendor
+            ? import(`../devices/${this.device.vendor}/${this.device.type}/device.vue`)
+            : import(`./${this.device.vendor}/${this.device.type}/consumer.vue`),
+        errorComponent: OpenwbConsumerDeviceConfigFallback,
+      });
     },
     updateConfiguration(event) {
       this.$emit("update:configuration", event);
