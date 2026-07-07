e0d613463afa6c051a0c5dc405e509fcd40f4522
Brett-S-OWB <brett.scott@openwb.de>
2026-07-07 17:32:23 +0200

    update consumer summary card

--- src/components/status/ConsumerSumCard.vue
+++ src/components/status/ConsumerSumCard.vue
@@ -5,7 +5,7 @@
       Alle Verbraucher
     </template>
     <template #header-right>
-      <span class="text-right"> {{ formatNumberTopic(baseTopic + "/get/power", 3, 3, 0.001) }}&nbsp;kW </span>
+      <span class="text-right"> {{ formatNumber(totalPower, 3, 3, 0.001) }}&nbsp;kW </span>
     </template>
     <openwb-base-card
       subtype="white"
@@ -15,38 +15,7 @@
     >
       <div class="row">
         <div class="col pr-0 text-right">Leistung</div>
-        <div class="col text-right text-monospace">
-          {{ formatNumberTopic(baseTopic + "/get/power", 3, 3, 0.001) }}&nbsp;kW
-        </div>
-      </div>
-    </openwb-base-card>
-    <openwb-base-card
-      subtype="white"
-      body-bg="white"
-      class="py-1 mb-2"
-      title="Zählerstände"
-    >
-      <div class="row justify-content-end">
-        <div class="col-4 text-right">Import</div>
-        <div class="col-4 text-right">Export</div>
-      </div>
-      <div class="row">
-        <div class="col text-right">Heute</div>
-        <div class="col-4 text-right text-monospace">
-          {{ formatNumberTopic(baseTopic + "/get/daily_imported", 3, 3, 0.001) }}&nbsp;kWh
-        </div>
-        <div class="col-4 text-right text-monospace">
-          {{ formatNumberTopic(baseTopic + "/get/daily_exported", 3, 3, 0.001) }}&nbsp;kWh
-        </div>
-      </div>
-      <div class="row">
-        <div class="col text-right">Gesamt</div>
-        <div class="col-4 text-right text-monospace">
-          {{ formatNumberTopic(baseTopic + "/get/imported", 3, 3, 0.001) }}&nbsp;kWh
-        </div>
-        <div class="col-4 text-right text-monospace">
-          {{ formatNumberTopic(baseTopic + "/get/exported", 3, 3, 0.001) }}&nbsp;kWh
-        </div>
+        <div class="col text-right text-monospace">{{ formatNumber(totalPower, 3, 3, 0.001) }}&nbsp;kW</div>
       </div>
     </openwb-base-card>
   </status-card>
@@ -71,19 +40,15 @@ export default {
   mixins: [ComponentState],
   data() {
     return {
-      mqttTopics: [
-        { topic: "openWB/consumer/get/power", writeable: false },
-        { topic: "openWB/consumer/get/imported", writeable: false },
-        { topic: "openWB/consumer/get/exported", writeable: false },
-        { topic: "openWB/consumer/get/daily_imported", writeable: false },
-        { topic: "openWB/consumer/get/daily_exported", writeable: false },
-      ],
+      mqttTopics: [{ topic: "openWB/consumer/+/get/power", writeable: false }],
     };
   },
   computed: {
-    baseTopic: {
+    totalPower: {
       get() {
-        return "openWB/consumer";
+        return Object.values(this.getWildcardTopics("openWB/consumer/+/get/power")).reduce((sum, power) => {
+          return sum + (typeof power === "number" ? power : 0);
+        }, 0);
       },
     },
   },
