d73b60fe23248abb859fff1f7b4e4b69e699815a
Alexander Hartung <alexander.hartung@openwb.de>
2026-07-09 12:48:14 +0200

    Add requested changes

--- src/components/io_actions/controllable_consumers/load_manager/io_action.vue
+++ src/components/io_actions/controllable_consumers/load_manager/io_action.vue
@@ -1,11 +1,5 @@
 <template>
-  <openwb-io-single-pattern
-    v-if="ioDevice?.type !== 'eebus' && value && value.length > 0"
-    v-model="value"
-    :contacts="ioDevice?.input?.digital || {}"
-  />
   <openwb-base-number-input
-    v-if="ioAction?.configuration && ioDevice?.type !== 'eebus'"
     title="maximale Bezugsleistung"
     unit="kW"
     min="0"
@@ -14,7 +8,16 @@
     :model-value="(ioAction.configuration.max_import_power || 0) / 1000"
     @update:model-value="updateConfiguration($event * 1000, 'configuration.max_import_power')"
   />
-  <hr v-if="ioDevice?.type !== 'eebus'" />
+  <openwb-base-number-input
+    title="maximale Bezugsleistung im Fehlerfall"
+    unit="kW"
+    min="0"
+    step="0.01"
+    required
+    :model-value="(ioAction.configuration.max_power_on_failure || 0) / 1000"
+    @update:model-value="updateConfiguration($event * 1000, 'configuration.max_power_on_failure')"
+  />
+  <hr />
   <openwb-base-select-input
     title="Anwenden auf..."
     :groups="availableDevices"
@@ -24,15 +27,9 @@
     @update:model-value="updateConfiguration($event, 'configuration.devices')"
   >
     <template #help>
-      Bitte die Ladepunkte und/oder digitale Ausgänge auswählen, welche berücksichtigt werden sollen. Es können mehrere
-      Einträge ausgewählt werden. Bei steuerbaren Verbrauchseinrichtungen, die über einen digitalen Ausgang angesteuert
-      werden, wird eine Leistung von 4,2 kW bei aktiver Dimmung angenommen. Ladepunkte werden so gesteuert, dass die
-      angegebene Leistung am EVU-Punkt nicht überschritten wird. Eigene PV-Erzeugung und vorhandene Speicher werden
-      berücksichtigt und zusätzlich genutzt.<br />
-      Zugeordnete digitale Ausgänge werden im nicht gedimmten Zustand aktiviert und im gedimmten Zustand deaktiviert. So
-      ist sichergestellt, dass bei einem Verbindungsabbruch oder einem Ausfall der Steuerung die Verbraucher nicht
-      ungewollt mit voller Leistung betrieben werden. Ein mit "NO" bezeichneter digitaler Ausgang ist also für den nicht
-      gedimmten Zustand geschlossen und bei aktivierter Dimmung geöffnet.
+      Bitte die Ladepunkte auswählen, die vom Lastmanager gesteuert werden sollen. Es können mehrere Einträge ausgewählt werden.
+      Die Ladepunkte werden so gesteuert, dass die konfigurierte Leistung am EVU-Punkt nicht überschritten wird.
+      Im Fehlerfall wird automatisch die konfigurierte Fehlerleistung ausgegeben.
     </template>
   </openwb-base-select-input>
 </template>
@@ -42,7 +39,7 @@ import OpenwbIoActionConfigMixin from "../../OpenwbIoActionConfigMixin.vue";
 import OpenwbIoSinglePattern from "../../OpenwbIoSinglePattern.vue";
 
 export default {
-  name: "IoActionLoadMananger",
+  name: "IoActionLoadManager",
   components: {
     OpenwbIoSinglePattern,
   },
