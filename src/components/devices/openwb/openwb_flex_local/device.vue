a6323aab2b701a0424b3efd79295d70c99c07431
Alexander Hartung <alexander.hartung@openwb.de>
2026-08-03 15:44:31 +0200

    Add Infotext

---
+++ src/components/devices/openwb/openwb_flex_local/device.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="device-openwb-flex-local">
+    <openwb-base-heading> Einstellungen für openWB-Flex-Local </openwb-base-heading>
+  </div>
+  <openwb-base-alert subtype="info">
+    Es können maximal 8 Zähler gleichzeitig ausgelesen werden.
+  </openwb-base-alert>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceOpenwbFlexLocal",
+  mixins: [DeviceConfigMixin],
+};
+</script>
