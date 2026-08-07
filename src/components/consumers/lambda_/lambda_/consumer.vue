5924756ade23b52c885f8f701237dda509a0dc37
SeaSpotter <florian.horch@gmail.com>
2026-08-07 18:04:10 +0200

    Revise Modbus TCP settings and remove surplus option
    Updated alert message for Modbus TCP configuration and removed PV surplus sending option.

--- src/components/consumers/lambda_/lambda_/consumer.vue
+++ src/components/consumers/lambda_/lambda_/consumer.vue
@@ -2,9 +2,10 @@
   <div class="device-lambda">
     <openwb-base-heading> Einstellungen für Lambda Wärmepumpe </openwb-base-heading>
     <openwb-base-alert subtype="info">
-      Modbus TCP muss in der Fachmannebene aktiviert sein. Soll openWB den PV-Überschuss senden, muss
-      zusätzlich am E-Manager der Wärmepumpe die Datenquelle auf "Modbus Client" umgestellt werden - ohne
-      diese Einstellung wertet die Wärmepumpe den gesendeten Wert nicht aus.
+      Modbus TCP muss in der Fachmannebene aktiviert sein. Ist als Verwendungszweck "Wärmepumpe in
+      Eigensteuerung" gewählt, sendet openWB den aktuellen Netzbezug an den E-Manager der
+      Wärmepumpe - dafür muss zusätzlich am E-Manager die Datenquelle auf "Modbus Client"
+      umgestellt werden, sonst wertet die Wärmepumpe den gesendeten Wert nicht aus.
     </openwb-base-alert>
     <openwb-base-text-input
       title="IP oder Hostname"
@@ -29,22 +30,6 @@
       max="255"
       @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
     />
-    <openwb-base-select-input
-      title="PV-Überschuss senden"
-      not-selected="Bitte auswählen"
-      :options="[
-        { value: true, text: 'ja' },
-        { value: false, text: 'nein' },
-      ]"
-      :model-value="device.configuration.send_values"
-      required
-      @update:model-value="updateConfiguration($event, 'configuration.send_values')"
-    >
-      <template #help>
-        Sendet den aktuellen PV-Überschuss an den E-Manager der Wärmepumpe. Setzt voraus, dass die
-        Datenquelle des E-Managers auf "Modbus Client" eingestellt ist (siehe Hinweis oben).
-      </template>
-    </openwb-base-select-input>
   </div>
 </template>
 
