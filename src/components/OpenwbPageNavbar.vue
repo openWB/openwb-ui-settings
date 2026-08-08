77556f5dc23aaea365a0eb50c4308edece7864a5
seaspotter <florian.horch@gmail.com>
2026-08-08 21:21:00 +0200

    refactor(security-ui): restore wildcard ACL subscription

--- src/components/OpenwbPageNavbar.vue
+++ src/components/OpenwbPageNavbar.vue
@@ -368,36 +368,6 @@ import ComponentState from "./mixins/ComponentState.vue";
 
 library.add(fasExternalLinkAlt);
 
-const SECURITY_ACCESS_TOPICS = [
-  "openWB/system/security/access/Settings",
-  "openWB/system/security/access/Status",
-  "openWB/system/security/access/ChargeLog",
-  "openWB/system/security/access/Chart",
-  "openWB/system/security/access/GeneralConfiguration",
-  "openWB/system/security/access/DisplayConfiguration",
-  "openWB/system/security/access/IdentificationConfiguration",
-  "openWB/system/security/access/GeneralChargeConfiguration",
-  "openWB/system/security/access/SurplusChargeConfiguration",
-  "openWB/system/security/access/ActiveBatControlConfiguration",
-  "openWB/system/security/access/HardwareInstallation",
-  "openWB/system/security/access/LoadManagementConfiguration",
-  "openWB/system/security/access/ForecastConfiguration",
-  "openWB/system/security/access/ChargePointInstallation",
-  "openWB/system/security/access/VehicleConfiguration",
-  "openWB/system/security/access/IoConfiguration",
-  "openWB/system/security/access/LegacySmartHomeConfiguration",
-  "openWB/system/security/access/InstallAssistant",
-  "openWB/system/security/access/TenantEnergyConfiguration",
-  "openWB/system/security/access/CloudConfiguration",
-  "openWB/system/security/access/MqttBridgeConfiguration",
-  "openWB/system/security/access/DebugConfiguration",
-  "openWB/system/security/access/Support",
-  "openWB/system/security/access/DataManagement",
-  "openWB/system/security/access/SecurityConfiguration",
-  "openWB/system/security/access/SystemConfiguration",
-  "openWB/system/security/access/LegalSettings",
-];
-
 export default {
   name: "OpenwbNavBar",
   components: {
@@ -406,7 +376,7 @@ export default {
   mixins: [ComponentState],
   data() {
     return {
-      mqttTopics: SECURITY_ACCESS_TOPICS.map((topic) => ({ topic, writeable: false })),
+      mqttTopics: [{ topic: "openWB/system/security/access/+", writeable: false }],
     };
   },
   computed: {
