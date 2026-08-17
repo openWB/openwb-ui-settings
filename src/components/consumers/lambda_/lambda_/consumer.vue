3c9f1d29f6b3f258f942affec0c3a6ea9d282aa2
SeaSpotter <florian.horch@gmail.com>
2026-08-17 11:45:35 +0200

    Update alert message in consumer.vue for clarity

--- src/components/consumers/lambda_/lambda_/consumer.vue
+++ src/components/consumers/lambda_/lambda_/consumer.vue
@@ -2,10 +2,10 @@
   <div class="device-lambda">
     <openwb-base-heading> Einstellungen für Lambda Wärmepumpe </openwb-base-heading>
     <openwb-base-alert subtype="info">
-      Modbus TCP muss in der Fachmannebene aktiviert sein. Bei Verwendung "Die Wärmepumpe übernimmt die Steuerung
-      selbst." sendet openWB den aktuellen Netzbezug an den E-Manager der Wärmepumpe - zusätzlich muss am E-Manager die
-      Datenquelle auf "Modbus Client" umgestellt werden, sonst wertet die Wärmepumpe den Wert nicht aus. Bei "Steuerung
-      durch Vorgabe einer Sollleistung" schreibt openWB stattdessen die Sollleistung in dasselbe Register, wodurch der
+      Modbus TCP muss in der Fachmannebene aktiviert sein. Bei Verwendung "	Wärmepumpe in Eigensteuerung" sendet 
+      openWB den aktuellen Netzbezug an den E-Manager der Wärmepumpe - zusätzlich muss am E-Manager die
+      Datenquelle auf "Modbus Client" umgestellt werden, sonst wertet die Wärmepumpe den Wert nicht aus. Bei 
+      "Stufenlos regelbar" schreibt openWB stattdessen die Sollleistung in dasselbe Register, wodurch der
       an die Wärmepumpe gemeldete Netzbezug verfälscht wird.
     </openwb-base-alert>
     <openwb-base-text-input
