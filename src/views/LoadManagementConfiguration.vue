09dabee26afeed439a370df3877d46e3e1356cf3
Alexander Hartung <alexander.hartung@openwb.de>
2026-08-18 09:10:35 +0200

    Refactor HC to use CounterMode enum

--- src/views/LoadManagementConfiguration.vue
+++ src/views/LoadManagementConfiguration.vue
@@ -362,7 +362,6 @@ export default {
         { topic: "openWB/consumer/+/module", writeable: false },
         { topic: "openWB/consumer/+/extra_meter", writeable: false },
         { topic: "openWB/counter/+/config/is_home_consumption_counter", writeable: true },
-        { topic: "openWB/counter/+/config/is_home_consumption_counter_auto", writeable: true },
         { topic: "openWB/counter/+/config/max_currents", writeable: true },
         { topic: "openWB/counter/+/config/max_power_errorcase", writeable: true },
         { topic: "openWB/counter/+/config/max_total_power", writeable: true },
@@ -637,33 +636,35 @@ export default {
       };
       this.loadManagementPriorityList = rename(this.loadManagementPriorityList);
     getHomeConsumptionCounterMode(counterId) {
-      const autoTopic = `openWB/counter/${counterId}/config/is_home_consumption_counter_auto`;
-      const boolTopic = `openWB/counter/${counterId}/config/is_home_consumption_counter`;
-      const autoValue = this.$store.state.mqtt[autoTopic];
-
+      const counterModeTopic = `openWB/counter/${counterId}/config/is_home_consumption_counter`;
+      const counterModeValue = this.$store.state.mqtt[counterModeTopic];
       // Frontend default: solange kein Wert vorhanden ist, wird "Automatisch" vorausgewählt.
-      if (autoValue === undefined || autoValue === null) {
+      if (counterModeValue === undefined || counterModeValue === null) {
         return "auto";
       }
-
-      if (autoValue === true) {
+      
+      if (counterModeValue === 0) {
+        return false;
+      }
+      else if (counterModeValue === 1) {
+        return true;
+      }
+      else {
         return "auto";
       }
-
-      return this.$store.state.mqtt[boolTopic] === true;
     },
     setHomeConsumptionCounterMode(counterId, value) {
-      const autoTopic = `openWB/counter/${counterId}/config/is_home_consumption_counter_auto`;
-      const boolTopic = `openWB/counter/${counterId}/config/is_home_consumption_counter`;
-
-      if (value === "auto") {
-        this.updateState(autoTopic, true);
-        this.updateState(boolTopic, false);
+      const counterModeTopic = `openWB/counter/${counterId}/config/is_home_consumption_counter`;
+      if (value === true) {
+        this.updateState(counterModeTopic, 1);
+        return;
+      }
+      else if (value === false) {
+        this.updateState(counterModeTopic, 0);
         return;
       }
 
-      this.updateState(autoTopic, false);
-      this.updateState(boolTopic, value === true);
+      this.updateState(counterModeTopic, 2);
     },
   },
 };
