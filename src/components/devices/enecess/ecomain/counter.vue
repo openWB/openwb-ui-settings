2357e0381c85a3219a2879bd27fe49e722793950
enecess <develop@enecess.com>
2026-08-19 17:15:39 +0800

    feat: add EcoMain device settings (#1018)
    Co-authored-by: facefunny <529611579@qq.com>

---
+++ src/components/devices/enecess/ecomain/counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="device-enecess-ecomain-counter">
+    <openwb-base-heading> Einstellungen für EcoMain EVU-Zähler </openwb-base-heading>
+    <openwb-base-alert subtype="info">
+      Die Phasenwerte und Gesamtwerte werden vom Hauptanschluss des EcoMain übernommen. Eine Frequenz wird vom EcoMain
+      nicht bereitgestellt.
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceEneCessEcoMainCounter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
---
+++ src/components/devices/enecess/ecomain/device.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="device-enecess-ecomain">
+    <openwb-base-heading> Einstellungen für enecess EcoMain </openwb-base-heading>
+    <openwb-base-alert subtype="info"> Softwareversion 138 oder höher ist erforderlich. </openwb-base-alert>
+    <openwb-base-text-input
+      title="IP-Adresse / Hostname"
+      subtype="host"
+      required
+      :model-value="device.configuration.ip_address"
+      @update:model-value="updateConfiguration($event, 'configuration.ip_address')"
+    />
+    <openwb-base-text-input
+      title="Seriennummer"
+      required
+      :model-value="device.configuration.serial_number"
+      @update:model-value="updateConfiguration($event, 'configuration.serial_number')"
+    />
+  </div>
+</template>
+
+<script>
+import DeviceConfigMixin from "../../DeviceConfigMixin.vue";
+
+export default {
+  name: "DeviceEneCessEcoMain",
+  mixins: [DeviceConfigMixin],
+};
+</script>
