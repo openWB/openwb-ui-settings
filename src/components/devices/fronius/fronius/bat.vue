58b2234dcdf148a21c845c3677491529a537aca8
ndrsnhs <andreas.neuhaus@openwb.de>
2026-08-26 17:55:48 +0200

    add username/password

--- src/components/devices/fronius/fronius/bat.vue
+++ src/components/devices/fronius/fronius/bat.vue
@@ -9,6 +9,27 @@
       :model-value="component.configuration.meter_id"
       @update:model-value="updateConfiguration($event, 'configuration.meter_id')"
     />
+    <openwb-base-text-input
+      title="Username"
+      required
+      :model-value="component.configuration.username"
+      @update:model-value="updateConfiguration($event, 'configuration.username')"
+    >
+      <template #help>
+        Für die aktive Speichersteuerung sind Nutzername und Passwort des Installateurszugangs erforderlich.
+      </template>
+    </openwb-base-text-input>
+    <openwb-base-text-input
+      title="Passwort"
+      subtype="password"
+      required
+      :model-value="component.configuration.password"
+      @update:model-value="updateConfiguration($event, 'configuration.password')"
+    >
+      <template #help>
+        Für die aktive Speichersteuerung sind Nutzername und Passwort des Installateurszugangs erforderlich.
+      </template>
+    </openwb-base-text-input>
   </div>
 </template>
 
