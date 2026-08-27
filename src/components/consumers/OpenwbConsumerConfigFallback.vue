6a39d9353633c4717375962b6f3df13fc8c9cbd6
Lutz Bender <github@lutz-bender.de>
2026-08-27 13:41:54 +0200

    remove wording "device"

--- src/components/consumers/OpenwbConsumerConfigFallback.vue
+++ src/components/consumers/OpenwbConsumerConfigFallback.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="consumer-device-fallback">
+  <div class="consumer-config-fallback">
     <openwb-base-alert
-      v-if="Object.keys(device.configuration).length == 0"
+      v-if="Object.keys(consumer.configuration).length == 0"
       subtype="info"
     >
-      Der Verbraucher-Typ "{{ device.type }}" bietet keine Einstellungen.
+      Der Verbraucher-Typ "{{ consumer.type }}" bietet keine Einstellungen.
     </openwb-base-alert>
     <div v-else>
       <openwb-base-alert subtype="warning">
-        Es wurde keine Konfigurationsseite für den Verbraucher-Typ "{{ device.type }}" gefunden. Die Einstellungen
+        Es wurde keine Konfigurationsseite für den Verbraucher-Typ "{{ consumer.type }}" gefunden. Die Einstellungen
         können als JSON direkt bearbeitet werden.
       </openwb-base-alert>
       <openwb-base-textarea
         title="Konfiguration"
         subtype="json"
-        :model-value="device.configuration"
+        :model-value="consumer.configuration"
         @update:model-value="updateConfiguration($event, 'configuration')"
       >
         <template #help> Bitte prüfe, ob die Eingaben richtig interpretiert werden. </template>
       </openwb-base-textarea>
       <openwb-base-alert subtype="info">
-        <pre>{{ JSON.stringify(device.configuration, undefined, 2) }}</pre>
+        <pre>{{ JSON.stringify(consumer.configuration, undefined, 2) }}</pre>
       </openwb-base-alert>
     </div>
   </div>
 </template>
 
 <script>
-import ConsumerDeviceConfigMixin from "./ConsumerDeviceConfigMixin.vue";
+import ConsumerConfigMixin from "./ConsumerConfigMixin.vue";
 
 export default {
-  name: "ConsumerDeviceFallback",
-  mixins: [ConsumerDeviceConfigMixin],
+  name: "OpenwbConsumerConfigFallback",
+  mixins: [ConsumerConfigMixin],
 };
 </script>
--- src/components/consumers/OpenwbConsumerConfigProxy.vue
+++ src/components/consumers/OpenwbConsumerConfigProxy.vue
@@ -1,19 +1,19 @@
 <template>
   <component
     :is="getComponent()"
-    :device="device"
+    :consumer="consumer"
     @update:configuration="updateConfiguration($event)"
   />
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
-import OpenwbConsumerDeviceConfigFallback from "./OpenwbConsumerDeviceConfigFallback.vue";
+import OpenwbConsumerConfigFallback from "./OpenwbConsumerConfigFallback.vue";
 
 export default {
   name: "OpenwbConsumerConfigProxy",
   props: {
-    device: { type: Object, required: true },
+    consumer: { type: Object, required: true },
   },
   emits: ["update:configuration"],
   data() {
@@ -23,14 +23,14 @@ export default {
   },
   methods: {
     getComponent() {
-      console.debug(`loading consumer config: ${this.device.vendor} / ${this.device.type}`);
-      const isSharedVendor = this.SHARED_DEVICE_VENDORS.includes(this.device.vendor);
+      console.debug(`loading consumer config: ${this.consumer.vendor} / ${this.consumer.type}`);
+      const isSharedVendor = this.SHARED_DEVICE_VENDORS.includes(this.consumer.vendor);
       return defineAsyncComponent({
         loader: () =>
           isSharedVendor
-            ? import(`../devices/${this.device.vendor}/${this.device.type}/device.vue`)
-            : import(`./${this.device.vendor}/${this.device.type}/consumer.vue`),
-        errorComponent: OpenwbConsumerDeviceConfigFallback,
+            ? import(`../devices/${this.consumer.vendor}/${this.consumer.type}/device.vue`)
+            : import(`./${this.consumer.vendor}/${this.consumer.type}/consumer.vue`),
+        errorComponent: OpenwbConsumerConfigFallback,
       });
     },
     updateConfiguration(event) {
