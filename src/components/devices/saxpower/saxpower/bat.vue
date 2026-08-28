6652b7c9b8c8536e13c853e037f8112ef590000a
ndrsnhs <andreas.neuhaus@openwb.de>
2026-08-28 12:34:21 +0200

    fix typo

--- src/components/devices/saxpower/saxpower/bat.vue
+++ src/components/devices/saxpower/saxpower/bat.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="device-saxpower-bat">
     <openwb-base-alert subtype="info">
-      Um die aktive Speichersteuerung zu nutzen müssen Register 43/44 vom Saxpower
+      Um die aktive Speichersteuerung zu nutzen, müssen Register 43/44 vom Saxpower
       Support freigeschaltet werden. Hierfür wird die Seriennummer benötigt.
     </openwb-base-alert>
   </div>
