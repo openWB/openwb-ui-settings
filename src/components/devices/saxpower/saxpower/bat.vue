46641e69b01bf1b28ef8277363ab5e2b0941e42d
ndrsnhs <andreas.neuhaus@openwb.de>
2026-08-28 12:32:17 +0200

    add info text

---
+++ src/components/devices/saxpower/saxpower/bat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="device-saxpower-bat">
+    <openwb-base-alert subtype="info">
+      Um die aktive Speichersteuerung zu nutzen müssen Register 43/44 vom Saxpower
+      Support freigeschaltet werden. Hierfür wird die Seriennummer benötigt.
+    </openwb-base-alert>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceSaxpowerBat",
+  mixins: [ComponentConfigMixin],
+};
+</script>
