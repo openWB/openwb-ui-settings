859b1dfe87dba4baaf8c703a19f5f374a74a829a
SeaSpotter <florian.horch@gmail.com>
2026-09-02 09:21:13 +0200

    Update vehicle.vue

--- src/components/vehicles/skoda/vehicle.vue
+++ src/components/vehicles/skoda/vehicle.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="vehicle-soc-myskoda">
     <openwb-base-alert subtype="info">
-      Für diese Anbindung wird ein persönlicher API-Schlüssel für die MyŠkoda Public API benötigt. Diesen unter
+      Für diese Anbindung wird ein persönlicher API-Schlüssel für die MyŠkoda Public API benötigt. Dieser wird in
+      der <strong>MyŠkoda-App</strong> erzeugt und verwaltet (App-Version mind. 8.16), nicht auf einer Website:
+      auf dem Handy mit installierter App
       <a href="https://go.skoda.eu/api-keys" target="_blank" rel="noopener">go.skoda.eu/api-keys</a>
-      anfordern.
+      öffnen, oder den QR-Code aus der
+      <a href="https://public.api.connect.skoda-auto.cz/docs" target="_blank" rel="noopener">offiziellen API-Doku</a>
+      mit dem Handy scannen. Der Key ist an die beim Erstellen ausgewählten Fahrzeuge gebunden und läuft ab -
+      rechtzeitig in der App erneuern.
     </openwb-base-alert>
     <openwb-base-text-input
       title="API-Schlüssel"
@@ -13,8 +18,7 @@
       @update:model-value="updateConfiguration($event, 'configuration.api_key')"
     >
       <template #help>
-        Persönlicher API-Schlüssel, angefordert unter
-        <a href="https://go.skoda.eu/api-keys" target="_blank" rel="noopener">go.skoda.eu/api-keys</a>.
+        In der MyŠkoda-App unter Schlüsselverwaltung erzeugt (App-Version mind. 8.16).
       </template>
     </openwb-base-text-input>
     <openwb-base-text-input
