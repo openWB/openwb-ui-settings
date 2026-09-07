2251e43ea1f9151dde71e2159602f5c5934d567a
ndrsnhs <156670705+ndrsnhs@users.noreply.github.com>
2026-09-07 12:15:56 +0200

    add DoxEss H3 Smart (#1057)

---
+++ src/components/devices/fox_ess/fox_ess_h3_smart/inverter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="device-foxess-h3-smart-inverter">
+    <openwb-base-heading> Einstellungen für FoxEss H3 Smart Wechselrichter </openwb-base-heading>
+    <openwb-base-number-input
+      title="Modbus ID"
+      required
+      :model-value="component.configuration.modbus_id"
+      min="1"
+      max="255"
+      @update:model-value="updateConfiguration($event, 'configuration.modbus_id')"
+    >
+      <template #help> Die Standard-Modbus-ID von FoxEss ist 247 </template>
+    </openwb-base-number-input>
+  </div>
+</template>
+
+<script>
+import ComponentConfigMixin from "../../ComponentConfigMixin.vue";
+
+export default {
+  name: "DeviceFoxEssH3SmartInverter",
+  mixins: [ComponentConfigMixin],
+};
+</script>
